7c530623e2fb1e0db687e1640984dcebc8cc7c88
Mio <mio@mio19.uk>
2025-09-11 12:53:47 +1000

    setup github actions

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 # see log: adb logcat | grep lindroid
 {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nix-github-actions.url = "github:nix-community/nix-github-actions";
+  inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
   inputs.robotnix.url = "github:nix-community/robotnix";
 
   outputs =
@@ -225,65 +228,69 @@
       );
     in
     {
-      gta4xlwifi = robotnix.lib.robotnixSystem (
-        args@{ config, pkgs, ... }:
-        common args
-          {
-            manufactor = "samsung";
-            kernel = "gta4xl";
-            defconfig = "arch/arm64/configs/exynos9611-gta4xlwifi_defconfig";
-            legacy = true;
-            ksu = false; # buggy
-            patch-daria = true;
-            patch-overlayfs = true;
-          }
-          {
-            device = "gta4xlwifi";
-            flavorVersion = "22.2";
-          }
-      );
-      enchilada = robotnix.lib.robotnixSystem (
-        args@{ config, pkgs, ... }:
-        common args
-          {
-            manufactor = "oneplus";
-            kernel = "sdm845";
-            defconfig = "arch/arm64/configs/enchilada_defconfig";
-          }
-          {
-            device = "enchilada";
-            flavorVersion = "22.2";
-            #flavorVersion = "23.0";
-          }
-      );
-      nx_tab = robotnix.lib.robotnixSystem (
-        args@{ config, pkgs, ... }:
-        common args
-          {
-            manufactor = "nintendo";
-            device-name = "nx";
-            kernel-name = "nvidia/kernel-4.9-nx";
-            defconfig = "arch/arm64/configs/tegra_android_defconfig";
-            lindroid = false;
-            legacy = true;
-            # ksu doesn't compile
-            #In file included from ../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c:1:
-            #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c: In function ‘track_throne’:
-            #../../../../../../kernel/nvidia/kernel-4.9-nx/include/linux/err.h:30:9: error: ‘fp’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
-            #   30 |  return (long) ptr;
-            #      |         ^~~~~~~~~~
-            #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c:356:15: note: ‘fp’ was declared here
-            #  356 |  struct file *fp;
-            #      |               ^~
-            #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c: At top level:
-            #cc1: error: unrecognized command line option ‘-Wno-gcc-compat’ [-Werror]
-            #cc1: all warnings being treated as errors
-            ksu = false;
-          }
-          {
-            device = "nx_tab";
-            flavorVersion = "22.2";
-          }
-      );
+      githubActions = nix-github-actions.lib.mkGithubMatrix { checks = self.robotnixConfigurations; };
+      # https://github.com/MatthewCroughan/nixcfg/blob/afab322e6da20cc038d8577dd4a365673702d183/flake.nix#L57
+      robotnixConfigurations = nixpkgs.lib.mapAttrs (n: v: robotnix.lib.robotnixSystem v) {
+        gta4xlwifi = (
+          args@{ config, pkgs, ... }:
+          common args
+            {
+              manufactor = "samsung";
+              kernel = "gta4xl";
+              defconfig = "arch/arm64/configs/exynos9611-gta4xlwifi_defconfig";
+              legacy = true;
+              ksu = false; # buggy
+              patch-daria = true;
+              patch-overlayfs = true;
+            }
+            {
+              device = "gta4xlwifi";
+              flavorVersion = "22.2";
+            }
+        );
+        enchilada = (
+          args@{ config, pkgs, ... }:
+          common args
+            {
+              manufactor = "oneplus";
+              kernel = "sdm845";
+              defconfig = "arch/arm64/configs/enchilada_defconfig";
+            }
+            {
+              device = "enchilada";
+              flavorVersion = "22.2";
+              #flavorVersion = "23.0";
+            }
+        );
+        nx_tab = (
+          args@{ config, pkgs, ... }:
+          common args
+            {
+              manufactor = "nintendo";
+              device-name = "nx";
+              kernel-name = "nvidia/kernel-4.9-nx";
+              defconfig = "arch/arm64/configs/tegra_android_defconfig";
+              lindroid = false;
+              legacy = true;
+              # ksu doesn't compile
+              #In file included from ../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c:1:
+              #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c: In function ‘track_throne’:
+              #../../../../../../kernel/nvidia/kernel-4.9-nx/include/linux/err.h:30:9: error: ‘fp’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
+              #   30 |  return (long) ptr;
+              #      |         ^~~~~~~~~~
+              #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c:356:15: note: ‘fp’ was declared here
+              #  356 |  struct file *fp;
+              #      |               ^~
+              #../../../../../../kernel/nvidia/kernel-4.9-nx/drivers/kernelsu/throne_tracker.c: At top level:
+              #cc1: error: unrecognized command line option ‘-Wno-gcc-compat’ [-Werror]
+              #cc1: all warnings being treated as errors
+              ksu = false;
+            }
+            {
+              device = "nx_tab";
+              flavorVersion = "22.2";
+            }
+        );
+      };
     };
 }
