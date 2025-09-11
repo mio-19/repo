bb23f940da97d4222120535694302f10fa20a61e
Mio <mio@mio19.uk>
2025-09-11 21:56:48 +1000

    fmt

--- common.nix
+++ common.nix
@@ -183,9 +183,9 @@ in
     ];
     # https://github.com/KernelSU-Next/KernelSU-Next/blob/next/kernel/Kconfig
     postPatch = ''
-      ${lib.optionalString (
-        config.lindroid && config.lindroid-drm
-      ) ''cp -r ${if config.legacy then lindroid-drm414 else lindroid-drm} drivers/lindroid-drm''}
+      ${lib.optionalString (config.lindroid && config.lindroid-drm)
+        ''cp -r ${if config.legacy then lindroid-drm414 else lindroid-drm} drivers/lindroid-drm''
+      }
       ${lib.optionalString config.ksu ''
         cp -r ${kernelsu}/kernel drivers/kernelsu
         chmod -R +w drivers/kernelsu
