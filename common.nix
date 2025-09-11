169a31a1050d1727b683dca2935c956f512f3467
Mio <mio@mio19.uk>
2025-09-12 00:53:03 +1000

    s

--- common.nix
+++ common.nix
@@ -191,6 +191,8 @@ in
       ${lib.optionalString config.ksu ''
         cp -r ${kernelsu}/kernel drivers/kernelsu
         chmod -R +w drivers/kernelsu
+        # KSU_VERSION = git rev-list --count HEAD
+        # 10000 + $(KSU_GIT_VERSION) + 200
         sed -i 's|-DKSU_VERSION=11998|-DKSU_VERSION=14630|' drivers/kernelsu/Makefile
         # https://kernelsu-next.github.io/webpage/pages/installation.html -> https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh
         printf "\nobj-\$(CONFIG_KSU) += kernelsu/\n" >> drivers/Makefile
