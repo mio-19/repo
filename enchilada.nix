777270416a51f31ab1088e59ada2ee045d23d701
Mio <mio@mio19.uk>
2025-09-12 22:39:03 +1000

    qwq

--- enchilada.nix
+++ enchilada.nix
@@ -9,7 +9,7 @@ args@{ config, pkgs, ... }:
   flavorVersion = "22.2";
   #flavorVersion = "23.0";
   legacy414 = true;
-  ksu = true;
+  ksu = false; # DIFFICULT TO COMPILE KSU FOR 4.9 kernel
   lindroid = false; # lindroid doesn't support 4.9?
   lindroid-drm = false; # /build/kernel/oneplus/sdm845/drivers/lindroid-drm/evdi_modeset.c:35:10: fatal error: 'drm/drm_gem_framebuffer_helper.h' file not found
 }
