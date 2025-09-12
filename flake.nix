e70ea3612a15665fbc3bd9395709b217296865b0
Mio <mio@mio19.uk>
2025-09-12 18:09:34 +1000

    lindroid = false; # lindroid doesn't support 4.9?

--- flake.nix
+++ flake.nix
@@ -49,6 +49,7 @@
               flavorVersion = "22.2";
               #flavorVersion = "23.0";
               legacy414 = true;
+              lindroid = false; # lindroid doesn't support 4.9?
               lindroid-drm = false; # /build/kernel/oneplus/sdm845/drivers/lindroid-drm/evdi_modeset.c:35:10: fatal error: 'drm/drm_gem_framebuffer_helper.h' file not found
             }
           );
