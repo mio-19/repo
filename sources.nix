8d8fb0066b19da985e8e32129ed4219fdde92415
Mio <mio@mio19.uk>
2025-09-14 01:15:33 +1000

    use https://github.com/Linux-on-droid/lindroid-drm-loopback.git

--- sources.nix
+++ sources.nix
@@ -6,9 +6,9 @@ args@{
 }:
 {
   lindroid-drm = pkgs.fetchgit {
-    #url = "https://github.com/Linux-on-droid/lindroid-drm-loopback.git";
-    url = "https://github.com/Linux-On-LineageOS/lindroid-drm-loopback.git";
-    rev = "01d020e58d5d09d70f37c28a062297a78d9b4f4e";
-    sha256 = "1w1g5h3zrhdq3zzk0l0yzy73mjnm9f7xm1qzb29479jcacnck5yk";
+    url = "https://github.com/Linux-on-droid/lindroid-drm-loopback.git";
+    #url = "https://github.com/Linux-On-LineageOS/lindroid-drm-loopback.git";
+    rev = "d44bfb83c7940f9adf8ee2714d05746f0fc86312";
+    sha256 = "0bdskvx2b135dlf1l1pf7c88ghsjmf4n0prl7z95xmvpsfizz1w9";
   };
 }
