79cc05f68df1c84308e0259bb39b0ab224586dcf
Mio <mio@mio19.uk>
2025-10-03 20:59:47 +1000

    try older lindroid-drm

--- sources.nix
+++ sources.nix
@@ -8,7 +8,7 @@ args@{
   lindroid-drm = pkgs.fetchgit {
     url = "https://github.com/Linux-on-droid/lindroid-drm-loopback.git";
     #url = "https://github.com/Linux-On-LineageOS/lindroid-drm-loopback.git";
-    rev = "6dc67b3128a54e387170b50c13037e4370d06751";
-    sha256 = "038s6wva9i6rz9kkz2p9b16172f05lwcnm8vxmwkxrjsb87ikw2b";
+    rev = "d44bfb83c7940f9adf8ee2714d05746f0fc86312"; # pin
+    sha256 = "0bdskvx2b135dlf1l1pf7c88ghsjmf4n0prl7z95xmvpsfizz1w9";
   };
 }
