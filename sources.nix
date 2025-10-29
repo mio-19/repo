da8a495b68d6f607c3b43dbd884326f7e3b08ba9
Mio <mio@mio19.uk>
2025-10-30 10:59:24 +1100

    bump

--- sources.nix
+++ sources.nix
@@ -6,10 +6,10 @@ args@{
 }:
 {
   lindroid-drm = pkgs.fetchgit {
-    #url = "https://github.com/Linux-on-droid/lindroid-drm-loopback.git";
-    url = "https://github.com/mio-19/lindroid-drm-loopback.git";
+    url = "https://github.com/Linux-on-droid/lindroid-drm-loopback.git";
+    #url = "https://github.com/mio-19/lindroid-drm-loopback.git";
     #url = "https://github.com/Linux-On-LineageOS/lindroid-drm-loopback.git";
-    rev = "bfa24f48033660e1f470842582e4b241d9622b4d";
-    sha256 = "1h9rnd939g5sff127p4mkjks7ddxdzwsr24mzq9bzwk6wakqkbrg";
+    rev = "53ecac1e2b49bbf947e9068f988779578cf803aa";
+    sha256 = "1hdvkaa045jk4gq9fixsdlxcdly86xkxwq5iqz87mgnpxjca5vnd";
   };
 }
