6bfc0a5ab46851b4d9c4354ab20fbeed69148685
Mio <mio@mio19.uk>
2025-11-12 16:35:40 +1100

    frameworks-base-DisableFlagSecure

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -6,6 +6,6 @@ args@{ config, pkgs, ... }:
   grapheneos.channel = "alpha";
   apps.fdroid.enable = true;
   source.dirs."frameworks/base".patches = [
-    ./services-core-isSecureLocked.patch
+    ./frameworks-base-DisableFlagSecure.patch
   ];
 }
