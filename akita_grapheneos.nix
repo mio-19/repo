ea49b2faa2540515ec6523ed2c720526a4a56d64
Mio <mio@mio19.uk>
2025-11-13 16:40:40 +1100

    #

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -6,6 +6,6 @@ args@{ config, pkgs, ... }:
   grapheneos.channel = "alpha";
   source.dirs."frameworks/base".patches = [
     # https://github.com/LSPosed/DisableFlagSecure/blob/4b3c477a06f05096af6da1a70941c12418e0d567/app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
-    ./frameworks-base-DisableFlagSecure.patch
+    #./frameworks-base-DisableFlagSecure.patch
   ];
 }
