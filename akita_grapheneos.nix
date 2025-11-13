7092d5afec0b571d683a01c80f8c05a172c0c6b3
Mio <mio@mio19.uk>
2025-11-13 18:36:03 +1100

    move commetns?

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -5,9 +5,6 @@ args@{ config, pkgs, ... }:
   device = "akita";
   grapheneos.channel = "alpha";
   source.dirs."frameworks/base".patches = [
-    # https://github.com/LSPosed/DisableFlagSecure/blob/4b3c477a06f05096af6da1a70941c12418e0d567/app/src/main/java/io/github/lsposed/disableflagsecure/DisableFlagSecure.java
-    #./frameworks-base-DisableFlagSecure.patch
-    # https://github.com/VarunS2002/Xposed-Disable-FLAG_SECURE/blob/f35b4c31f7bc593c4d8142d86885b8a35ef5708b/app/src/main/java/com/varuns2002/disable_flag_secure/DisableFlagSecure.kt
     ./Disable-FLAG_SECURE.patch
   ];
 }
