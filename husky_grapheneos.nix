2837c4301e99c9daf3b55193125175102f09cfab
Mio <mio@mio19.uk>
2026-02-20 11:46:03 +1100

    correct path

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -8,11 +8,12 @@ args@{ config, pkgs, lib, ... }:
     ./Disable-FLAG_SECURE.patch
     #./No-gestural-navigation-hint-bar.patch
   ];
+  # check in nix repl (import ./.).gosSign.husky.config.source.dirs."device/google/shusky-kernels/6.1"
   source.dirs."device/google/shusky-kernels/6.1" = lib.mkForce {
     src = pkgs.fetchgit {
       url = "https://github.com/mio-19/device_google_shusky-kernels_6.1.git";
-      rev = "4f21c869fb7aabd8e1e500ab64bd2919ba39e90a";
-      hash = "sha256-JjwWMnJAXVEl586CxUns229v59geql8aJZns1d9I7ZY=";
+      rev = "d2fb020dd95df3d7ddb216a0d109371250e5f40c";
+      hash = "sha256-VqiAWZ8JPMgTtdHpNFB3CFxJ3JtO4PwOQ8YnM3+hcbc=";
       fetchLFS = true;
     };
   };
