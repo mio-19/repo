035e0da3294414f1868b3e473716aa6ad66d4d6d
Mio <mio@mio19.uk>
2026-02-20 13:34:46 +1100

    husky kernel bump

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -12,8 +12,8 @@ args@{ config, pkgs, lib, ... }:
   source.dirs."device/google/shusky-kernels/6.1" = lib.mkForce {
     src = pkgs.fetchgit {
       url = "https://github.com/mio-19/device_google_shusky-kernels_6.1.git";
-      rev = "d2fb020dd95df3d7ddb216a0d109371250e5f40c";
-      hash = "sha256-VqiAWZ8JPMgTtdHpNFB3CFxJ3JtO4PwOQ8YnM3+hcbc=";
+      rev = "ca5c9ade50d99db93d43d72b8702eec2adf849f5";
+      hash = "sha256-mtwb/f6NBW9qNvY+De6AwHoMQaCdU/0zTaJ4WiXqo5Q=";
       fetchLFS = true;
     };
   };
