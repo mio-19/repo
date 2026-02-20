fe205d4d518abdcc8f5d874924f2833e1d1d472d
Mio <mio@mio19.uk>
2026-02-20 11:39:46 +1100

    comment

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -8,6 +8,7 @@ args@{ config, pkgs, ... }:
     ./Disable-FLAG_SECURE.patch
     #./No-gestural-navigation-hint-bar.patch
   ];
+  # this is enough to override. check in nix repl (import ./.).gosSign.husky.config.source.dirs."device/google/shusky-kernels/6.1"
   source.dirs."device/google/shusky-kernels/6.1".src = pkgs.fetchgit {
     url = "https://github.com/mio-19/device_google_shusky-kernels_6.1.git";
     rev = "4f21c869fb7aabd8e1e500ab64bd2919ba39e90a";
