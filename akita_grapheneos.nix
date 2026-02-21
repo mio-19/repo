c22dc5571f8568308950e1c4e4f636bd4c764731
Mio <mio@mio19.uk>
2026-02-21 22:41:53 +1100

    0001-akita-increase-default-zram-and-set-writeback-to-4G.patch

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -3,4 +3,7 @@ args@{ config, pkgs, ... }:
   imports = [ ./gos.nix ];
   device = "akita";
   signing.avb.size = 2048;
+  source.dirs."vendor/adevtool".patches = [
+    ./0001-akita-increase-default-zram-and-set-writeback-to-4G.patch
+  ];
 }
