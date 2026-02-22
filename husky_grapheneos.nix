e5920cd96945a09b2beca7cfdf15d467ded5ece6
Mio <mio@mio19.uk>
2026-02-22 21:34:03 +1100

    https://t.me/linux_on_droid/26461

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -55,6 +55,14 @@ args@{
     postPatch = ''
       sed -i 's|android.hardware.graphics.common-V5|android.hardware.graphics.common-V7|' interfaces/composer/Android.bp
     '';
+    patches = with pkgs; [
+      (fetchpatch {
+        # https://t.me/linux_on_droid/26461
+        name = "perspectived: exempt from init dir mutation";
+        url = "https://github.com/yaap/vendor_lindroid/commit/762067a0e9506af5127cd95d96acc3725c05b7d8.patch";
+        hash = "sha256-7LTKEWHAXG+EJC5zW1kXdMr1Nrsh0Jr3+3p6pmoSVX4=";
+      })
+    ];
   };
   source.dirs."external/lxc".src = pkgs.fetchFromGitHub {
     owner = "Linux-on-droid";
@@ -70,4 +78,12 @@ args@{
     rev = "419f3ff6736e01cb0e579f65a34c85cfa7de578b";
     hash = "sha256-h9QmJ/uZ2sHMGX3/UcxD+xe/myONacKwoBhmn0RK5sI=";
   };
+  source.dirs."system/sepolicy".patches = with pkgs; [
+    (fetchpatch {
+      # https://t.me/linux_on_droid/26461
+      name = "private/domain: add new attr for relaxing a dir init neverallow";
+      url = "https://github.com/yaap/system_sepolicy/commit/d48ff481d9651cedb435a9974648e5c9a81fe211.patch";
+      hash = "sha256-bDUOj+NwErgqjM+abpF6ITaz3+GHag+qMoZXsfCv+KI=";
+    })
+  ];
 }
