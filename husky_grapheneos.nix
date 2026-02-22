2895ce6ac1bf17c6142522e71b0f942ab09ac923
Mio <mio@mio19.uk>
2026-02-23 10:16:43 +1100

    TODO: for user - ERROR: permissive domains not allowed in user builds https://t.me/linux_on_droid/5984

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -31,11 +31,12 @@ args@{
     };
   };
   signing.avb.size = 4096;
-  #variant = "userdebug";
+  # TODO: for user - ERROR: permissive domains not allowed in user builds https://t.me/linux_on_droid/5984
+  variant = "userdebug";
   # Making userdebug builds with ro.adb.secure=1 to have root access via ADB with the rest of the security model intact is officially supported by GrapheneOS. Using Magisk massively rolls back the OS security model and is strongly discouraged. Using ADB on a production device isn't recommended with or without root, but it's officially supported if you want to do it. If you only grant ADB access to the computer you use for building and signing the OS, it's not a big deal. You need to be aware that you need to heavily secure that computer and shouldn't use it for anything else though. https://news.ycombinator.com/item?id=40250160
   source.dirs."vendor/adevtool".postPatch = ''
-    #echo '
-    #PRODUCT_SYSTEM_PROPERTIES += ro.adb.secure=1' >> config/mk/google_devices/device/husky/device.mk
+    echo '
+    PRODUCT_SYSTEM_PROPERTIES += ro.adb.secure=1' >> config/mk/google_devices/device/husky/device.mk
     sed -i '/vendor\/adevtool\/config\/mk\/google_devices\/platform\/zuma\/product-common\.mk/a $(call inherit-product, vendor/lindroid/lindroid.mk)' config/mk/google_devices/device/husky/device.mk
   '';
   # lindroid:
