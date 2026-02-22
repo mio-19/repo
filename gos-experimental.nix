6a60b95a29ce70e6805468673b2460195446fc76
Mio <mio@mio19.uk>
2026-02-22 14:15:26 +1100

    soong: Add equivalent for LOCAL_EXPORT_CFLAGS

--- gos-experimental.nix
+++ gos-experimental.nix
@@ -13,4 +13,12 @@ args@{
       hash = "sha256-QFrwYPZ6Vt/KoUaHWd7w72KB7s4SeYQ2xFoymgi7JeE=";
     };
   };
+  source.dirs."build/soong".patches = with pkgs; [
+    # required for export_cflags
+    (fetchpatch {
+      name = "soong: Add equivalent for LOCAL_EXPORT_CFLAGS";
+      url = "https://github.com/LineageOS/android_build_soong/commit/25aa912a6cadab1f47753c719b05c6596022c5f8.patch";
+      hash = "sha256-o+nQlmA8RoVRxrSYqSJoJQh725RVJLp5J9SUILX0bHA=";
+    })
+  ];
 }
