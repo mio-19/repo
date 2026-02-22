b56a6ad82ac8fb1cc7bd72ecfc6133b2963fdc25
Mio <mio@mio19.uk>
2026-02-22 14:24:05 +1100

    vendor-adevtool-8G-4G-100.patch

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -5,6 +5,6 @@ args@{ config, pkgs, ... }:
   signing.avb.size = 2048;
   source.dirs."vendor/adevtool".patches = [
     ./0001-akita-increase-default-zram-and-set-writeback-to-4G.patch
-    ./vendor-adevtool-4G-100.patch
+    ./vendor-adevtool-8G-4G-100.patch
   ];
 }
