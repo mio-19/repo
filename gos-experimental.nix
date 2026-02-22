cca75e1c2fdcab21c61a48bd0764cee016a4c5c7
Mio <mio@mio19.uk>
2026-02-22 14:09:05 +1100

    fixup

--- gos-experimental.nix
+++ gos-experimental.nix
@@ -5,12 +5,12 @@ args@{
   ...
 }:
 {
-  source.dirs."bootable/recovery".patches = with pkgs; [
-    (fetchFromGitHub {
+  source.dirs."bootable/recovery" = lib.mkForce {
+    src = pkgs.fetchFromGitHub {
       owner = "LineageOS";
       repo = "android_bootable_recovery";
       rev = "833e2948bfe4bbfbf73796391a9336850108e7bd";
       hash = "sha256-QFrwYPZ6Vt/KoUaHWd7w72KB7s4SeYQ2xFoymgi7JeE=";
-    })
-  ];
+    };
+  };
 }
