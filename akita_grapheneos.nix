6d6958c69d7419b6ce5d4620114d11e1d5824478
Mio <mio@mio19.uk>
2026-02-22 14:27:50 +1100

    #not needed#

--- akita_grapheneos.nix
+++ akita_grapheneos.nix
@@ -4,7 +4,7 @@ args@{ config, pkgs, ... }:
   device = "akita";
   signing.avb.size = 2048;
   source.dirs."vendor/adevtool".patches = [
-    ./0001-akita-increase-default-zram-and-set-writeback-to-4G.patch
+    #not needed# ./0001-akita-increase-default-zram-and-set-writeback-to-4G.patch
     ./vendor-adevtool-8G-4G-100.patch
   ];
 }
