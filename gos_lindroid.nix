77a74ee8bfaf0c5ae3fbc5c6e27d7d1316fcdfbc
Mio <mio@mio19.uk>
2026-02-23 13:38:40 +1100

    https://github.com/droidng/android_system_sepolicy/commit/b491a953db739aa2daffabc13c9b153d329013ee#diff-8d88eb632967032ab70dc52ece6ec958fb13d818e22d3513130ae177993b2fd7

--- gos_lindroid.nix
+++ gos_lindroid.nix
@@ -53,7 +53,7 @@ args@{
       url = "https://github.com/yaap/system_sepolicy/commit/d48ff481d9651cedb435a9974648e5c9a81fe211.patch";
       hash = "sha256-bDUOj+NwErgqjM+abpF6ITaz3+GHag+qMoZXsfCv+KI=";
     })
-    # ERROR: permissive domains not allowed in user builds https://t.me/linux_on_droid/5984 - You could just vandalize the check if you really want user builds: https://t.me/linux_on_droid/13590
+    # ERROR: permissive domains not allowed in user builds https://t.me/linux_on_droid/5984 - You could just vandalize the check if you really want user builds: https://t.me/linux_on_droid/13590 - https://github.com/droidng/android_system_sepolicy/commit/b491a953db739aa2daffabc13c9b153d329013ee#diff-8d88eb632967032ab70dc52ece6ec958fb13d818e22d3513130ae177993b2fd7
     ./platform_system_sepolicy-remove-check-of-permissive-.patch
   ];
 }
