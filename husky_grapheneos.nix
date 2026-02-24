f7700c4f9ca48f00ac050085aa808a8127546304
Mio <mio@mio19.uk>
2026-02-24 21:47:22 +1100

    lindroid-partial6 works too

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -12,15 +12,6 @@ args@{
   device = "husky";
   # check in nix repl (import ./.).gosSign.husky.config.source.dirs."device/google/shusky-kernels/6.1"
   source.dirs."device/google/shusky-kernels/6.1" = lib.mkForce {
-    /*
-      src = pkgs.fetchFromGitHub {
-        # pixel8pro-stock-3840Hz.patch pixel8pro-stock-fix-attempt3.patch lindroid-partial5 0ac686b9e81ba331c2ad9b420fd21262a80daaa4.patch  3dcc884c689681dda2d9ad24a9e219013f70cfe8.patch a72032ecf33c63d8a4abb64b08c1a0b847c82a32.patch
-        owner = "forked-by-mio";
-        repo = "device_google_shusky-kernels_6.1";
-        rev = "1b067623ef23c5cd711b313960f308d78ea5e9fd";
-        hash = "sha256-d6vyvG4mxsoY46U5MK3OjB/wFxfI+/6QVhm0fjqaDW8=";
-      };
-    */
     src = pkgs.fetchFromGitHub {
       # pixel8pro-stock-3840Hz.patch pixel8pro-stock-fix-attempt3.patch lindroid-partial6 0ac686b9e81ba331c2ad9b420fd21262a80daaa4.patch  3dcc884c689681dda2d9ad24a9e219013f70cfe8.patch a72032ecf33c63d8a4abb64b08c1a0b847c82a32.patch
       owner = "forked-by-mio";
--- kernel/README.md
+++ kernel/README.md
@@ -61,31 +61,6 @@ TODO: consider CONFIG_POSIX_MQUEUE
 
 ### lindroid-partial3 - testing only some parts to figure out why bootloop
 
-seem like CONFIG_SYSVIPC=y and CONFIG_DRM_LINDROID_EVDI=y cause problem.
-
-lindroid-partial5
-```zsh
-tee -a private/devices/google/shusky/shusky_defconfig << 'EOF'
-
-# lindroid
-CONFIG_UTS_NS=y
-CONFIG_PID_NS=y
-CONFIG_USER_NS=y
-CONFIG_NET_NS=y
-CONFIG_CGROUP_DEVICE=y
-CONFIG_CGROUP_FREEZER=y
-CONFIG_DRM_LINDROID_EVDI=y
-CONFIG_TMPFS_POSIX_ACL=y
-EOF
-
-sed -i '/^# CONFIG_PID_NS is not set$/d' aosp/arch/arm64/configs/gki_defconfig
-sed -i '/^CONFIG_NAMESPACES=y$/a CONFIG_USER_NS=y' aosp/arch/arm64/configs/gki_defconfig
-
-sed -i '/^  __fsnotify_parent$/a\  from_kuid' aosp/android/abi_gki_aarch64_pixel
-sed -i '/^  from_kuid$/a\  from_kuid_munged' aosp/android/abi_gki_aarch64_pixel
-sed -i '/^  mac_pton$/a\  make_kuid' aosp/android/abi_gki_aarch64_pixel
-```
-
 lindroid-partial6
 ```zsh
 tee -a private/devices/google/shusky/shusky_defconfig << 'EOF'
