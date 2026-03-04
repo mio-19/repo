7f7df9b48a8ea498744608c327eeafa1f5b5df82
Mio <mio@mio19.uk>
2026-03-04 20:48:40 +1100

    kernelSU105.nix

--- grapheneos_husky_kernel.nix
+++ grapheneos_husky_kernel.nix
@@ -76,34 +76,7 @@ let
 
   lindroidDrm = sources.lindroid_drm_loopback.src;
 
-  kernelSU = fetchgit {
-    url = "https://github.com/tiann/KernelSU.git";
-    rev = "v1.0.5";
-    hash = "sha256-Te9/UkFuOhCKel3/tEvntKLJZmzfLmejy54PI4363Xc=";
-    leaveDotGit = true;
-    deepClone = true;
-    # populate values that require us to use git and deepClone. By doing this in postFetch we
-    # can delete .git afterwards and maintain better reproducibility of the src.
-    postFetch = ''
-      cd "$out"
-
-      git rev-parse HEAD > "$out/COMMIT"
-      date -u -d "@$(git log -1 --pretty=%ct)" "+%Y-%m-%dT%H:%M:%SZ" > "$out/SOURCE_DATE_EPOCH"
-
-      KSU_GIT_VERSION="$(git rev-list --count HEAD)"
-      KSU_VERSION="$((10000 + KSU_GIT_VERSION + 200))"
-      echo "$KSU_VERSION" > "$out/KSU_VERSION"
-
-      echo "KSU_VERSION=$KSU_VERSION"
-
-      substituteInPlace "$out/kernel/Makefile" \
-        --replace-fail \
-        "ccflags-y += -DKSU_VERSION=16" \
-        "ccflags-y += -DKSU_VERSION=$KSU_VERSION"
-
-      find "$out" -name .git -print0 | xargs -0 rm -rf
-    '';
-  };
+  kernelSU = import ./kernelSU105.nix { inherit fetchgit; };
 in
 stdenvNoCC.mkDerivation {
   pname = "grapheneos-husky-kernel";
