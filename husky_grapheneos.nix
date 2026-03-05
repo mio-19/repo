1a1e56ab0b9cb397460233ec34962e0a441b6135
Mio <mio@mio19.uk>
2026-03-05 18:12:12 +1100

    enableLindroid = false;

--- husky_grapheneos.nix
+++ husky_grapheneos.nix
@@ -4,16 +4,20 @@ args@{
   lib,
   ...
 }:
+let
+  enableLindroid = false;
+in
 {
   imports = [
     ./gos.nix
     ./gos_lindroid.nix
     #./gos_userdebug.nix
   ];
+  enableLindroid = enableLindroid;
   device = "husky";
   # check in nix repl (import ./.).gosSign.husky.config.source.dirs."device/google/shusky-kernels/6.1"
   source.dirs."device/google/shusky-kernels/6.1" = lib.mkForce {
-    src = pkgs-unstable.callPackage ./grapheneos_husky_kernel.nix { };
+    src = pkgs-unstable.callPackage ./grapheneos_husky_kernel.nix { inherit enableLindroid; };
   };
   signing.avb.size = 4096;
   stateVersion = "2";
