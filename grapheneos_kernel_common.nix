f8c4f574f1cf8a3e729d2c5236f989f9f273e440
Mio <mio@mio19.uk>
2026-03-10 19:40:28 +1100

    fmt

--- grapheneos_kernel_common.nix
+++ grapheneos_kernel_common.nix
@@ -135,36 +135,36 @@ stdenvNoCC.mkDerivation {
         } > aosp/KernelSU/kernel/Makefile
       ''}
 
-    ${lib.optionalString enableLindroid ''
-      # lindroid steps
-      sed -i "/^# CONFIG_PID_NS is not set$/d" aosp/arch/arm64/configs/gki_defconfig
-      sed -i "/^CONFIG_NAMESPACES=y$/a CONFIG_USER_NS=y" aosp/arch/arm64/configs/gki_defconfig
-      sed -i "/^CONFIG_INTERCONNECT=y$/a CONFIG_DRM_LINDROID_EVDI=y" aosp/arch/arm64/configs/gki_defconfig
-      sed -i "/^CONFIG_TMPFS=y$/a CONFIG_TMPFS_POSIX_ACL=y" aosp/arch/arm64/configs/gki_defconfig
-      sed -i "/^CONFIG_CPUSETS=y$/a CONFIG_CGROUP_DEVICE=y" aosp/arch/arm64/configs/gki_defconfig
-      sed -i "/^CONFIG_UAPI_HEADER_TEST=y$/a CONFIG_SYSVIPC=y" aosp/arch/arm64/configs/gki_defconfig
+      ${lib.optionalString enableLindroid ''
+        # lindroid steps
+        sed -i "/^# CONFIG_PID_NS is not set$/d" aosp/arch/arm64/configs/gki_defconfig
+        sed -i "/^CONFIG_NAMESPACES=y$/a CONFIG_USER_NS=y" aosp/arch/arm64/configs/gki_defconfig
+        sed -i "/^CONFIG_INTERCONNECT=y$/a CONFIG_DRM_LINDROID_EVDI=y" aosp/arch/arm64/configs/gki_defconfig
+        sed -i "/^CONFIG_TMPFS=y$/a CONFIG_TMPFS_POSIX_ACL=y" aosp/arch/arm64/configs/gki_defconfig
+        sed -i "/^CONFIG_CPUSETS=y$/a CONFIG_CGROUP_DEVICE=y" aosp/arch/arm64/configs/gki_defconfig
+        sed -i "/^CONFIG_UAPI_HEADER_TEST=y$/a CONFIG_SYSVIPC=y" aosp/arch/arm64/configs/gki_defconfig
 
-      sed -i "/^  __fsnotify_parent$/a\\  from_kuid" aosp/android/abi_gki_aarch64_pixel
-      sed -i "/^  from_kuid$/a\\  from_kuid_munged" aosp/android/abi_gki_aarch64_pixel
-      sed -i "/^  mac_pton$/a\\  make_kuid" aosp/android/abi_gki_aarch64_pixel
+        sed -i "/^  __fsnotify_parent$/a\\  from_kuid" aosp/android/abi_gki_aarch64_pixel
+        sed -i "/^  from_kuid$/a\\  from_kuid_munged" aosp/android/abi_gki_aarch64_pixel
+        sed -i "/^  mac_pton$/a\\  make_kuid" aosp/android/abi_gki_aarch64_pixel
 
-      rm -rf aosp/drivers/lindroid-drm
-      cp -r ${lindroidDrm} aosp/drivers/lindroid-drm
-      echo "obj-y += lindroid-drm/" >> aosp/drivers/Makefile
-      sed -i "/endmenu/i\\source \"drivers/lindroid-drm/Kconfig\"" aosp/drivers/Kconfig
+        rm -rf aosp/drivers/lindroid-drm
+        cp -r ${lindroidDrm} aosp/drivers/lindroid-drm
+        echo "obj-y += lindroid-drm/" >> aosp/drivers/Makefile
+        sed -i "/endmenu/i\\source \"drivers/lindroid-drm/Kconfig\"" aosp/drivers/Kconfig
 
-      (
-        cd aosp
-        apply_patch ${./kernel/0ac686b9e81ba331c2ad9b420fd21262a80daaa4.patch}
-        apply_patch ${./kernel/3dcc884c689681dda2d9ad24a9e219013f70cfe8.patch}
-        apply_patch ${./kernel/a72032ecf33c63d8a4abb64b08c1a0b847c82a32.patch}
-      )
+        (
+          cd aosp
+          apply_patch ${./kernel/0ac686b9e81ba331c2ad9b420fd21262a80daaa4.patch}
+          apply_patch ${./kernel/3dcc884c689681dda2d9ad24a9e219013f70cfe8.patch}
+          apply_patch ${./kernel/a72032ecf33c63d8a4abb64b08c1a0b847c82a32.patch}
+        )
 
-      ${lib.optionalString enableDaria ''
-        apply_patch ${./kernel/0001-daria.patch}
+        ${lib.optionalString enableDaria ''
+          apply_patch ${./kernel/0001-daria.patch}
+        ''}
+        apply_patch ${./kernel/sidharth-hack.patch}
       ''}
-      apply_patch ${./kernel/sidharth-hack.patch}
-    ''}
 
 
       ${
