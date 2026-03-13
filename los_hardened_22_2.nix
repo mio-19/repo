ad164d66434b05e2b848ea8e8ee1e28a754de84e
Mio <mio@mio19.uk>
2026-03-13 15:44:00 +1100

    22_2: disable bionic patches all together for now

--- los_hardened_22_2.nix
+++ los_hardened_22_2.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 let
@@ -33,7 +34,10 @@ in
     (mkAxpPatch "android_art/0001-constify_JNINativeMethod.patch")
   ];
 
-  source.dirs."bionic".patches = [
+  # robotnix-lineage_enchilada> error: libc's ABI has INCOMPATIBLE CHANGES. Please check compatibility report at: out/soong/.intermediates/bionic/libc/libc/android_arm64_armv8-a_shared_apex10000/libc.so.llndk.abidiff
+  # robotnix-lineage_enchilada> error: Please update ABI references with: $ANDROID_BUILD_TOP/development/vndk/tools/header-checker/utils/create_reference_dumps.py --lib libc --lib-variant LLNDK --release bp1a
+  # disable bionic patches all together for now
+  source.dirs."bionic".patches = lib.mkIf false [
     (mkAxpPatch "android_bionic/0013-fix-undefined-out-of-bounds-accesses-in-sched.h.patch")
     (mkAxpPatch "android_bionic/0014-replace-vla-formatting-with-dprintf-like-function.patch")
     (mkAxpPatch "android_bionic/0015-increase-default-pthread-stack-to-8mib-on-64-bit.patch")
