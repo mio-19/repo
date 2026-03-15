2a3938fb9ddcb230f7af1dcb13d27cca0c690bf9
Mio <mio@mio19.uk>
2026-03-15 21:57:10 +1100

    wthIWLAN = false;

--- gts7l.nix
+++ gts7l.nix
@@ -7,6 +7,10 @@ args@{
 }:
 let
   withIMS = args.withIMS or (config.device == "gts7l");
+  # Keep IWLAN disabled by default so we can prioritize stable VoLTE bring-up
+  # on Samsung RIL before adding WFC/epdg complexity.
+  # https://github.com/phhusson/ims/issues/26
+  wthIWLAN = false;
 in
 {
   imports = [
@@ -34,7 +38,7 @@ in
 
   # FLOSS IMS integration:
   # https://github.com/phhusson/ims/issues/22
-  resources."frameworks/base/core/res" = lib.mkIf withIMS {
+  resources."frameworks/base/core/res" = lib.mkIf (withIMS && wthIWLAN) {
     config_wlan_data_service_package = "com.google.android.iwlan";
     config_wlan_network_service_package = "com.google.android.iwlan";
     config_qualified_networks_service_package = "com.android.telephony.qns";
@@ -43,16 +47,27 @@ in
     config_ims_mmtel_package = "me.phh.ims";
   };
 
-  product.additionalProductPackages = lib.mkIf withIMS [
+  product.additionalProductPackages = lib.mkIf (withIMS && wthIWLAN) [
     "Iwlan"
     "QualifiedNetworksService"
   ];
 
-  product.extraConfig = lib.mkIf withIMS ''
-    PRODUCT_PRODUCT_PROPERTIES += persist.dbg.volte_avail_ovr=1
-    PRODUCT_PRODUCT_PROPERTIES += persist.dbg.wfc_avail_ovr=1
-    PRODUCT_PRODUCT_PROPERTIES += persist.dbg.allow_ims_off=1
-  '';
+  product.extraConfig = lib.mkIf withIMS (
+    ''
+      PRODUCT_PRODUCT_PROPERTIES += persist.dbg.volte_avail_ovr=1
+      PRODUCT_PRODUCT_PROPERTIES += persist.dbg.allow_ims_off=1
+    ''
+    + (
+      if wthIWLAN then
+        ''
+          PRODUCT_PRODUCT_PROPERTIES += persist.dbg.wfc_avail_ovr=1
+        ''
+      else
+        ''
+          PRODUCT_PRODUCT_PROPERTIES += persist.dbg.wfc_avail_ovr=0
+        ''
+    )
+  );
 
   apps.prebuilt.FlossIMS = lib.mkIf withIMS {
     apk = pkgs.fetchurl {
