9e1f826f9a10c305036a00f1a7bb0b7e33f27bef
Mio <mio@mio19.uk>
2026-03-21 20:24:47 +1100

    app/gadgetbridge: add source build

--- app/default.nix
+++ app/default.nix
@@ -284,6 +284,10 @@
         androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
       };
 
+      gadgetbridge = pkgs.callPackage ./gadgetbridge {
+        androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
+      };
+
       meditrak = pkgs.callPackage ./meditrak {
         androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
       };
@@ -685,6 +689,27 @@
             '';
           }
           {
+            appId = "nodomain.freeyourgadget.gadgetbridge";
+            apkPath = "${gadgetbridge}/gadgetbridge.apk";
+            metadataYml = ''
+              Categories:
+                - Connectivity
+                - Health & Fitness
+              License: Apache-2.0
+              WebSite: https://gadgetbridge.org/
+              SourceCode: https://codeberg.org/Freeyourgadget/Gadgetbridge
+              IssueTracker: https://codeberg.org/Freeyourgadget/Gadgetbridge/issues
+              Changelog: https://codeberg.org/Freeyourgadget/Gadgetbridge/releases
+              AutoName: Gadgetbridge
+              Summary: Companion app for wearable devices
+              Description: |-
+                Gadgetbridge is a libre companion app for wearable devices.
+
+                This package is built from source and follows the current
+                F-Droid mainline build, including the Fossil HR asset build step.
+            '';
+          }
+          {
             appId = "projects.medicationtracker";
             apkPath = "${meditrak}/meditrak.apk";
             metadataYml = ''
@@ -950,6 +975,14 @@
           name = "sign-termux";
           apkPath = "${termux}/termux.apk";
           defaultOut = "termux-signed.apk";
+        };
+      });
+
+      packages.gadgetbridge = gadgetbridge.overrideAttrs (_: {
+        passthru.signScript = mkSignScript {
+          name = "sign-gadgetbridge";
+          apkPath = "${gadgetbridge}/gadgetbridge.apk";
+          defaultOut = "gadgetbridge-signed.apk";
         };
       });
 
