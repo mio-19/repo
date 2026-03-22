53bb81b40e43543f6a3d60d49eb83163c9fe6b5a
Mio <mio@mio19.uk>
2026-03-22 19:30:04 +1100

    sha256-rjHyjy4QtbukDdPrxCzJY6DzXhHHcPHkYHrnWzHIvLQ

--- grapheneos_kernel_common.nix
+++ grapheneos_kernel_common.nix
@@ -183,7 +183,7 @@ stdenvNoCC.mkDerivation {
           apply_patch ${
             fetchpatch {
               url = "https://raw.githubusercontent.com/ravindu644/Droidspaces-OSS/refs/heads/main/Documentation/resources/kernel-patches/GKI/02.fix_restore%20cgroup%20file%20prefix%20handling%20.patch";
-              hash = "";
+              hash = "sha256-rjHyjy4QtbukDdPrxCzJY6DzXhHHcPHkYHrnWzHIvLQ";
             }
           }
           # patch duplicated with lindroid ./kernel/0ac686b9e81ba331c2ad9b420fd21262a80daaa4.patch
