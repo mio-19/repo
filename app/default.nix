abce861be65a88bab153c2981f932d74ea2360f0
Mio <mio@mio19.uk>
2026-03-22 22:30:32 +1100

    emacs not build on darwin

--- app/default.nix
+++ app/default.nix
@@ -556,34 +556,6 @@
             '';
           }
           {
-            appId = "org.gnu.emacs";
-            apkPath = "${emacs}/emacs.apk";
-            metadataYml = ''
-              Categories:
-                - Development
-                - Text Editor
-                - Writing
-              License: GPL-3.0-or-later
-              WebSite: https://www.gnu.org/software/emacs/
-              SourceCode: https://git.savannah.gnu.org/cgit/emacs.git/tree/
-              IssueTracker: https://debbugs.gnu.org/
-              Changelog: https://git.savannah.gnu.org/cgit/emacs.git/tree/etc/NEWS?h=master
-              Donate: https://my.fsf.org/donate/
-              AutoName: Emacs
-              Summary: GNU Emacs with Termux shared user ID support
-              Description: |-
-                GNU Emacs is an extensible, customizable, free/libre text
-                editor and Lisp environment.
-
-                This build is compiled from source from the current Emacs 31.0.50
-                development snapshot and configured with the shared user ID `com.termux`,
-                so it can access the files and executables of the Termux app
-                from this repo when both are installed and signed together.
-
-                Install Termux first, then install this Emacs build.
-            '';
-          }
-          {
             appId = "org.lsposed.lspatch";
             apkPath = "${lspatch-manager}/lspatch-manager.apk";
             metadataYml = ''
@@ -821,6 +793,34 @@
           }
         ]
         ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+          {
+            appId = "org.gnu.emacs";
+            apkPath = "${emacs}/emacs.apk";
+            metadataYml = ''
+              Categories:
+                - Development
+                - Text Editor
+                - Writing
+              License: GPL-3.0-or-later
+              WebSite: https://www.gnu.org/software/emacs/
+              SourceCode: https://git.savannah.gnu.org/cgit/emacs.git/tree/
+              IssueTracker: https://debbugs.gnu.org/
+              Changelog: https://git.savannah.gnu.org/cgit/emacs.git/tree/etc/NEWS?h=master
+              Donate: https://my.fsf.org/donate/
+              AutoName: Emacs
+              Summary: GNU Emacs with Termux shared user ID support
+              Description: |-
+                GNU Emacs is an extensible, customizable, free/libre text
+                editor and Lisp environment.
+
+                This build is compiled from source from the current Emacs 31.0.50
+                development snapshot and configured with the shared user ID `com.termux`,
+                so it can access the files and executables of the Termux app
+                from this repo when both are installed and signed together.
+
+                Install Termux first, then install this Emacs build.
+            '';
+          }
           # cannot build on darwin due to stdenv
           {
             appId = "org.koreader.launcher.fdroid";
