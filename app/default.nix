08dd515fe7ac0a6aecbad207517a67b2e61157d7
Mio <mio@mio19.uk>
2026-03-24 17:59:34 +1100

    app: add termux-x11

--- app/default.nix
+++ app/default.nix
@@ -304,6 +304,10 @@
         androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
       };
 
+      termuxX11 = pkgs.callPackage ./termux-x11 {
+        androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
+      };
+
       kernelsu = pkgs.callPackage ./kernelsu {
         androidSdkBuilder = inputs.android-nixpkgs.sdk.${system};
       };
@@ -879,6 +883,31 @@
             '';
           }
           {
+            appId = "com.termux.x11";
+            apkPath = "${termuxX11}/termux-x11.apk";
+            metadataYml = ''
+              Categories:
+                - Development
+              License: GPL-3.0-only
+              WebSite: https://termux.com
+              SourceCode: https://github.com/termux/termux-x11
+              IssueTracker: https://github.com/termux/termux-x11/issues
+              Changelog: https://github.com/termux/termux-x11/releases/tag/nightly
+              Donate: https://termux.com/donate.html
+              OpenCollective: Termux
+              AutoName: Termux:X11
+              Summary: X11 server add-on for Termux
+              Description: |-
+                Termux:X11 is the X11 server companion app for Termux.
+                This package is built from source from the upstream master
+                branch at commit 3376f0ed5f5c7cf4ba960df218a00c6cc053ffb7.
+
+                F-Droid does not currently ship metadata for this application,
+                so this repo follows the upstream nightly debug universal APK
+                build layout instead.
+            '';
+          }
+          {
             appId = "org.gnu.emacs";
             apkPath = "${emacs}/emacs.apk";
             metadataYml = ''
@@ -1188,6 +1217,14 @@
           name = "sign-termux-styling";
           apkPath = "${termux-styling}/termux-styling.apk";
           defaultOut = "termux-styling-signed.apk";
+        };
+      });
+
+      packages.termux-x11 = termuxX11.overrideAttrs (_: {
+        passthru.signScript = mkSignScript {
+          name = "sign-termux-x11";
+          apkPath = "${termuxX11}/termux-x11.apk";
+          defaultOut = "termux-x11-signed.apk";
         };
       });
 
