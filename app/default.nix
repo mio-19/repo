e1d5b99ec1f7ec8e0159cd36e32f3840f130b375
Mio <mio@mio19.uk>
2026-03-24 17:54:29 +1100

    Add GameNative to F-Droid repo

--- app/default.nix
+++ app/default.nix
@@ -774,6 +774,24 @@
             '';
           }
           {
+            appId = "app.gamenative";
+            apkPath = "${gamenative}/gamenative.apk";
+            metadataYml = ''
+              Categories:
+                - Games
+              License: GPL-3.0-only
+              SourceCode: https://github.com/utkarshdalal/GameNative
+              IssueTracker: https://github.com/utkarshdalal/GameNative/issues
+              Changelog: https://github.com/utkarshdalal/GameNative/releases
+              AutoName: GameNative
+              Summary: Android launcher for running Windows games
+              Description: |-
+                GameNative is an Android launcher for running Windows games with
+                integrated container, Steam, and compatibility-layer management.
+                This package is built from source.
+            '';
+          }
+          {
             appId = "moe.koiverse.archivetune";
             apkPath = "${archivetune}/archivetune.apk";
             metadataYml = ''
