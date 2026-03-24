0cbbb47902a8d3f746671c7c6e5ab8cade0a27e3
Mio <mio@mio19.uk>
2026-03-24 21:58:32 +1100

    # did not apply

--- app/thunderbird/default.nix
+++ app/thunderbird/default.nix
@@ -37,11 +37,14 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   patches = [
-    (fetchpatch {
-      name = "feat: configure notification actions";
-      url = "https://github.com/thunderbird/thunderbird-android/pull/10301.patch";
-      hash = "sha256-VXmW7qZktMeoFDtmijw/mJLT1yCPgfM5BytrFy89rr8=";
-    })
+    /*
+      # did not apply
+      (fetchpatch {
+        name = "feat: configure notification actions";
+        url = "https://github.com/thunderbird/thunderbird-android/pull/10301.patch";
+        hash = "sha256-VXmW7qZktMeoFDtmijw/mJLT1yCPgfM5BytrFy89rr8=";
+      })
+    */
     # Remove foojay JDK auto-provisioner, glean telemetry module,
     # googleplay funding module, and app-k9mail from project settings.
     # Replaces maven.mozilla.org with jitpack.io (keeping jitpack content filter).
