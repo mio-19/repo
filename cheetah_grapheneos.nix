da39586482cc1e7e74f4ed8991b2e3669bcd5f7c
Mio <mio@mio19.uk>
2026-03-25 10:04:52 +1100

    inherit (config) enableLindroid enableDroidspaces;

--- cheetah_grapheneos.nix
+++ cheetah_grapheneos.nix
@@ -18,7 +18,9 @@ in
   source.dirs."device/google/pantah-kernels/6.1" = lib.mkForce {
     src =
       let
-        src = pkgs-unstable.callPackage ./grapheneos_pantah_kernel.nix { inherit enableLindroid; };
+        src = pkgs-unstable.callPackage ./grapheneos_pantah_kernel.nix {
+          inherit (config) enableLindroid enableDroidspaces;
+        };
       in
       assert src.version == config.grapheneos.release;
       src;
--- tangorpro_grapheneos.nix
+++ tangorpro_grapheneos.nix
@@ -18,7 +18,9 @@ in
   source.dirs."device/google/tangorpro-kernels/6.1" = lib.mkForce {
     src =
       let
-        src = pkgs-unstable.callPackage ./grapheneos_tangorpro_kernel.nix { inherit enableLindroid; };
+        src = pkgs-unstable.callPackage ./grapheneos_tangorpro_kernel.nix {
+          inherit (config) enableLindroid enableDroidspaces;
+        };
       in
       assert src.version == config.grapheneos.release;
       src;
