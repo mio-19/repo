fcbb1f1a03c591caf3d85bedb68f15d6c1592ed9
Mio <mio@mio19.uk>
2026-03-25 12:40:11 +1100

    lintVitalAnalyzeRelease

--- app/gamenative/default.nix
+++ app/gamenative/default.nix
@@ -76,7 +76,7 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   gradleFlags = [
-    "-xlintVitalRelease"
+    "-xlintVitalAnalyzeRelease"
     "-Dorg.gradle.java.installations.auto-download=false"
     "-Dorg.gradle.java.installations.paths=${jdk17_headless}"
     "-Dandroid.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2"
