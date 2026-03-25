3457154f5b491050f418fa0f15feeb533a11e26f
Mio <mio@mio19.uk>
2026-03-25 22:51:29 +1100

    sha256-hvpgtJ5ZxEpSqtrVInfpOKpX8YbdMLdPYl3hrmILVEQ=

--- app/bilibili-cn/default.nix
+++ app/bilibili-cn/default.nix
@@ -11,7 +11,7 @@ let
     # https://www.apkmirror.com/apk/bilibili/bilibili-%e5%93%94%e5%93%a9%e5%93%94%e5%93%a9/bilibili-all-your-fav-videos-8-71-0-release/bilibili-all-your-fav-videos-8-71-0-2-android-apk-download/
     name = "tv.danmaku.bili-8.71.0.apk";
     url = "https://web.archive.org/web/20260325114822/https://eb5e7388c3df147b74dd2379b7cf8323.r2.cloudflarestorage.com/downloadprod/wp-content/uploads/2025/11/07/691ec7d10fc45/tv.danmaku.bili_8.71.0-8710600_minAPI23%28armeabi-v7a%2Cx86%29%28nodpi%29_apkmirror.com.apk?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=72a5ba3a0b8a601e535d5525f12f8177%2F20260325%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260325T114811Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=17fa4e48203ee87ac65361f33874bdad6acc369659c3883b70294a3871118d22";
-    hash = "";
+    hash = "sha256-hvpgtJ5ZxEpSqtrVInfpOKpX8YbdMLdPYl3hrmILVEQ=";
   };
 
 in
