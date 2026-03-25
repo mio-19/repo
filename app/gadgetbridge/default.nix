e60b71d2c910d838507c4d716660d9009878e063
Mio <mio@mio19.uk>
2026-03-25 13:31:25 +1100

    fixup?

--- app/gadgetbridge/default.nix
+++ app/gadgetbridge/default.nix
@@ -93,7 +93,8 @@ stdenv.mkDerivation (finalAttrs: {
   '';
 
   gradleFlags = [
-    "-xlintVitalRelease"
+    "-xlintVitalBanglejsRelease"
+    "-xlintVitalMainlineRelease"
     "-Dorg.gradle.java.installations.auto-download=false"
     "-Dorg.gradle.java.installations.paths=${jdk21}"
     "-Dandroid.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/36.0.0/aapt2"
