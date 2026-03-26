b77057afdb128d9ca3adb7cc4fcfe3d76116e951
Mio <mio@mio19.uk>
2026-03-26 13:10:36 +1100

    sha256-5hTU/KR/ins7uz5uf/DbZc3U+nFV9mBT64QkAiwJskY=

--- app/bilibili-cn/default.nix
+++ app/bilibili-cn/default.nix
@@ -8,10 +8,10 @@
 }:
 let
   bilibiliApk = fetchurl {
-    # https://www.apkmirror.com/apk/bilibili/bilibili-%e5%93%94%e5%93%a9%e5%93%94%e5%93%a9/bilibili-all-your-fav-videos-8-71-0-release/bilibili-all-your-fav-videos-8-71-0-2-android-apk-download/
-    name = "tv.danmaku.bili-apkmirror-8.71.0.apk";
-    url = "https://web.archive.org/web/20260325114822if_/https://eb5e7388c3df147b74dd2379b7cf8323.r2.cloudflarestorage.com/downloadprod/wp-content/uploads/2025/11/07/691ec7d10fc45/tv.danmaku.bili_8.71.0-8710600_minAPI23%28armeabi-v7a%2Cx86%29%28nodpi%29_apkmirror.com.apk?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=72a5ba3a0b8a601e535d5525f12f8177%2F20260325%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260325T114811Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=17fa4e48203ee87ac65361f33874bdad6acc369659c3883b70294a3871118d22";
-    hash = "sha256-hvpgtJ5ZxEpSqtrVInfpOKpX8YbdMLdPYl3hrmILVEQ=";
+    # https://www.apkmirror.com/apk/bilibili/bilibili-哔哩哔哩/bilibili-all-your-fav-videos-8-71-0-release/bilibili-all-your-fav-videos-8-71-0-3-android-apk-download/
+    name = "tv.danmaku.bili-apkmirror-8.71.0-arm64-v8a.apk";
+    url = "https://web.archive.org/web/20260326020743if_/https://eb5e7388c3df147b74dd2379b7cf8323.r2.cloudflarestorage.com/downloadprod/wp-content/uploads/2025/11/75/691ec673cdddf/tv.danmaku.bili_8.71.0-8710600_minAPI23%28arm64-v8a%29%28nodpi%29_apkmirror.com.apk?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=72a5ba3a0b8a601e535d5525f12f8177%2F20260326%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260326T020701Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Signature=ae2a0023e1bdb42d981e9f8e52b8332c999de82a13fdd34c797f907bd4ab6e56";
+    hash = "sha256-5hTU/KR/ins7uz5uf/DbZc3U+nFV9mBT64QkAiwJskY=";
   };
 in
 stdenv.mkDerivation {
