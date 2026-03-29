1a2d8d4cab4e9576985f611a9a78f5325973987f
Mio <mio@mio19.uk>
2026-03-29 14:26:09 +1100

    Revert "haven 4.0.0"
    This reverts commit 65fb8b614b2065f52aa7b65fe45b971ee180f134.

--- app/by-name-apk/haven/package.nix
+++ app/by-name-apk/haven/package.nix
@@ -164,14 +164,14 @@ let
     in
     {
       pname = "haven";
-      version = "4.0.0";
+      version = "3.28.0";
 
       src = fetchFromGitHub {
         owner = "GlassOnTin";
         repo = "Haven";
         tag = "v${finalAttrs0.version}";
         fetchSubmodules = true;
-        hash = "sha256-7mf64vDAK98dEfEOWXzKL22bMJYi52cLA5U0pNeot1s=";
+        hash = "sha256-eMEFubFeok8R9rLQk8xYdLnldumXJBpazxKAOndFhI0=";
       };
 
       patches = [
