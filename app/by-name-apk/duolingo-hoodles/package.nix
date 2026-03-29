27748d75af2691e4a21385460263407fc2254679
Mio <mio@mio19.uk>
2026-03-29 16:58:48 +1100

    https://apkpure.com/duolingo-language-lessons/com.duolingo/download/6.66.5

--- app/by-name-apk/duolingo-hoodles/package.nix
+++ app/by-name-apk/duolingo-hoodles/package.nix
@@ -19,6 +19,7 @@ let
         s.build-tools-35-0-0
       ]);
 
+      # https://apkpure.com/duolingo-language-lessons/com.duolingo/download/6.66.5
       duolingoXapk = fetchurl {
         name = "duolingo-6.66.5.xapk";
         url = "https://web.archive.org/web/20260329035920if_/https://d-e02.winudf.com/b/XAPK/Y29tLmR1b2xpbmdvXzIyOTNfZTVhYjFmOGY?_fn=RHVvbGluZ286IExhbmd1YWdlIExlc3NvbnNfNi42Ni41X0FQS1B1cmUueGFwaw&_p=Y29tLmR1b2xpbmdv&download_id=1237403174754415&is_hot=true&k=c7bac36b8b194fc6fcfe8c3bccc381b469c9f50b&uu=https%3A%2F%2Fd-07.winudf.com%2Fb%2FXAPK%2FY29tLmR1b2xpbmdvXzIyOTNfZTVhYjFmOGY%3Fk%3De8d3d257f004040d51eb2cbf0c3a437769c9f50b";
