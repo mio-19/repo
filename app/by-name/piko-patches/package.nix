a3285fd109d75594daef8fda775f51740475f8a1
Mio <mio@mio19.uk>
2026-03-31 17:43:51 +1100

    more share morphe-patcher-src

--- app/by-name/piko-patches/package.nix
+++ app/by-name/piko-patches/package.nix
@@ -10,6 +10,7 @@
   morphe-library-m2,
   apktool-src,
   multidexlib2-src,
+  morphe-patcher-src,
 }:
 let
   androidSdk = androidSdkBuilder (s: [
@@ -28,14 +29,6 @@ let
       hash = "sha256-8XcSmKcPbbWina9iN4xOGKF/wzybprFDYuDN9AYQOA0=";
       defaultJava = jdk21;
     }).wrapped;
-
-  # https://github.com/crimera/piko/blob/e0d2a7be9bb92173825bb7043cd9ed92986132b9/gradle/libs.versions.toml#L2
-  morphe-patcher-src = fetchFromGitHub {
-    owner = "MorpheApp";
-    repo = "morphe-patcher";
-    rev = "v1.3.2";
-    hash = "sha256-KxWdkgiRN4mFb4auibSpMKUydE7ZaAMPGhow7Pq5Y1A=";
-  };
 
   arsclib-src = fetchFromGitHub {
     owner = "MorpheApp";
@@ -60,57 +53,54 @@ stdenv.mkDerivation (finalAttrs: {
   gradleUpdateTask = finalAttrs.gradleBuildTask;
 
   postUnpack = ''
-        root="$PWD"
-        cp -a ${morphe-patcher-src} "$root/morphe-patcher"
-        chmod -R u+w "$root/morphe-patcher"
-        cp -a ${arsclib-src} "$root/ARSCLib"
-        chmod -R u+w "$root/ARSCLib"
-        cp -a ${apktool-src} "$root/Apktool"
-        chmod -R u+w "$root/Apktool"
+    root="$PWD"
+    cp -a ${morphe-patcher-src} "$root/morphe-patcher"
+    chmod -R u+w "$root/morphe-patcher"
+    cp -a ${arsclib-src} "$root/ARSCLib"
+    chmod -R u+w "$root/ARSCLib"
+    cp -a ${apktool-src} "$root/Apktool"
+    chmod -R u+w "$root/Apktool"
 
-        cp -a ${multidexlib2-src} "$root/multidexlib2"
-        chmod -R u+w "$root/multidexlib2"
+    cp -a ${multidexlib2-src} "$root/multidexlib2"
+    chmod -R u+w "$root/multidexlib2"
 
-        substituteInPlace "$sourceRoot/settings.gradle.kts" \
-          --replace-fail 'id("app.morphe.patches") version "1.1.1"' 'id("app.morphe.patches") version "1.2.0"' \
-          --replace-fail '
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/MorpheApp/registry")
-                credentials {
-                    username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("GITHUB_ACTOR"))
-                    password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("GITHUB_TOKEN"))
-                }
+    substituteInPlace "$sourceRoot/settings.gradle.kts" \
+      --replace-fail 'id("app.morphe.patches") version "1.1.1"' 'id("app.morphe.patches") version "1.2.0"' \
+      --replace-fail '
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
+            credentials {
+                username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("GITHUB_ACTOR"))
+                password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("GITHUB_TOKEN"))
             }
+        }
     ' '
             maven { url = uri("file://" + System.getenv("MORPHE_PLUGIN_M2")) }
             maven { url = uri("file://" + System.getenv("MORPHE_LIBRARY_M2")) }
     '
-        patch -d "$root/morphe-patcher" -p0 < ${./morphe-patcher.patch}
-        patch -d "$root/morphe-patcher" -p0 < ${./morphe-patcher-settings.patch}
-
-        printf '%s\n' \
-          "" \
-          '    // Added by Nix build: include ARSCLib as composite build.' \
-          '    val arsclibDir = file("../ARSCLib")' \
-          '    if (arsclibDir.exists()) {' \
-          '        includeBuild(arsclibDir) {' \
-          '            dependencySubstitution {' \
-          '                substitute(module("com.github.MorpheApp:ARSCLib")).using(project(":"))' \
-          '            }' \
-          '        }' \
-          '    }' \
-          "" \
-          '    // Added by Nix build: include morphe-patcher as composite build.' \
-          '    val patcherDir = file("../morphe-patcher")' \
-          '    if (patcherDir.exists()) {' \
-          '        includeBuild(patcherDir) {' \
-          '            dependencySubstitution {' \
-          '                substitute(module("app.morphe:morphe-patcher")).using(project(":"))' \
-          '            }' \
-          '        }' \
-          '    }' \
-          >> "$sourceRoot/settings.gradle.kts"
+    printf '%s\n' \
+      "" \
+      '    // Added by Nix build: include ARSCLib as composite build.' \
+      '    val arsclibDir = file("../ARSCLib")' \
+      '    if (arsclibDir.exists()) {' \
+      '        includeBuild(arsclibDir) {' \
+      '            dependencySubstitution {' \
+      '                substitute(module("com.github.MorpheApp:ARSCLib")).using(project(":"))' \
+      '            }' \
+      '        }' \
+      '    }' \
+      "" \
+      '    // Added by Nix build: include morphe-patcher as composite build.' \
+      '    val patcherDir = file("../morphe-patcher")' \
+      '    if (patcherDir.exists()) {' \
+      '        includeBuild(patcherDir) {' \
+      '            dependencySubstitution {' \
+      '                substitute(module("app.morphe:morphe-patcher")).using(project(":"))' \
+      '            }' \
+      '        }' \
+      '    }' \
+      >> "$sourceRoot/settings.gradle.kts"
   '';
 
   mitmCache = gradle.fetchDeps {
