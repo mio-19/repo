8f8bae6390041db05fd779e9c2d24931d51fc9fb
Mio <mio@mio19.uk>
2026-04-02 20:49:26 +1100

    cleanup termux-x11

--- app/by-name-apk/termux-x11/package.nix
+++ app/by-name-apk/termux-x11/package.nix
@@ -21,132 +21,13 @@ let
       shortRev = builtins.substring 0 7 rev;
       version = "unstable-2026-02-18";
 
-      rootSrc = fetchFromGitHub {
+      src = fetchFromGitHub {
         owner = "termux";
         repo = "termux-x11";
         inherit rev;
-        hash = "sha256-kT1/qawEvFswUjO4VGmSLUTyA3V4Bl6IaeTMZoGcauU=";
+        fetchSubmodules = true;
+        hash = "sha256-/S5tja9wFN2/XjNSxhIDIU6CY80W9MUAIPQ8aLtR9uk=";
       };
-
-      bzip2Src = fetchzip {
-        url = "https://gitlab.com/bzip2/bzip2/-/archive/66c46b8c9436613fd81bc5d03f63a61933a4dcc3/bzip2-66c46b8c9436613fd81bc5d03f63a61933a4dcc3.tar.gz";
-        hash = "sha256-m3AdOsVGpeG6gmp2q5tmqNtd3ZTUCQl0feLIleuGkOA=";
-      };
-
-      libepoxySrc = fetchzip {
-        url = "https://github.com/anholt/libepoxy/archive/1b6d7db184bb1a0d9af0e200e06a0331028eaaae.tar.gz";
-        hash = "sha256-so6HfH12qn+wf1jkd2ny8eVUiLjHRaXvPW1WtTMwtxQ=";
-      };
-
-      libfontencSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libfontenc/-/archive/780d1f6f192a331de7b298a96e23ebc44d2a884b/libfontenc-780d1f6f192a331de7b298a96e23ebc44d2a884b.tar.gz";
-        hash = "sha256-upChhVST27M5h2KKCcb5bN5MZrgIxgDthrwHH96ZNwc=";
-      };
-
-      libtirpcSrc = fetchzip {
-        url = "https://github.com/alisw/libtirpc/archive/5ca4ca92f629d9d83e83544b9239abaaacf0a527.tar.gz";
-        hash = "sha256-USqQ8qx1CEUGMLKvTxQOTTrH77HmaLOdS9gZ7ohuhO8=";
-      };
-
-      libx11Src = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libx11/-/archive/59917d28a3c41ad22d6fc52e323cafe2cdd596d5/libx11-59917d28a3c41ad22d6fc52e323cafe2cdd596d5.tar.gz";
-        hash = "sha256-6tkeyzR8c9/ZMnbeesddtbVV33UZAEEkJ/99RIoTzVw=";
-      };
-
-      libxauSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxau/-/archive/a9c65683e68b3a4349afee5d7673b393fb924d2e/libxau-a9c65683e68b3a4349afee5d7673b393fb924d2e.tar.gz";
-        hash = "sha256-zSj0btY9hz/OWTpzqVP+cHWDqKrBL4UkexOwp8B+OXU=";
-      };
-
-      libxcvtSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxcvt/-/archive/bfca4a27f9e8bada2469573653da75536c578946/libxcvt-bfca4a27f9e8bada2469573653da75536c578946.tar.gz";
-        hash = "sha256-le2TVWVN2cA4HwZ1h/T+mFPA7/bH1CIvSycKIGbNmVs=";
-      };
-
-      libxdmcpSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxdmcp/-/archive/cd27f35c08cd8a7b14cef31d3edc021e638aab7f/libxdmcp-cd27f35c08cd8a7b14cef31d3edc021e638aab7f.tar.gz";
-        hash = "sha256-Mapm1oH4pZL9juBNIXErC4bmi85Oqaoj+mnCSsN3jvk=";
-      };
-
-      libxfontSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxfont/-/archive/67520db20c6cc37927ae3a212f357e2dd26143f5/libxfont-67520db20c6cc37927ae3a212f357e2dd26143f5.tar.gz";
-        hash = "sha256-JcjuAnjukzsSxmjylVmrhADksAY8k97Nvgh+Cisq9zk=";
-      };
-
-      libxkbfileSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxkbfile/-/archive/39a5f8e67615f443e76146769d5f5f9abc5ebd2f/libxkbfile-39a5f8e67615f443e76146769d5f5f9abc5ebd2f.tar.gz";
-        hash = "sha256-9CeKYvB/7z2JMHit46rXgTHiiTW9o3KFf2TPhNeisZ0=";
-      };
-
-      libxshmfenceSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxshmfence/-/archive/c8acc32ffec83765c8388ba75f172bcd7892c3f9/libxshmfence-c8acc32ffec83765c8388ba75f172bcd7892c3f9.tar.gz";
-        hash = "sha256-RndmQz489tnkWHAII8stmsvBH961t2CFSyra8I2BVq4=";
-      };
-
-      libxtransSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/lib/libxtrans/-/archive/cf05ba4a10c90da2c63805a5375e983b174e28b0/libxtrans-cf05ba4a10c90da2c63805a5375e983b174e28b0.tar.gz";
-        hash = "sha256-+V7qrITaYqqC6wFo28jhM3oxpACk1GoEYgK0FRRQJqY=";
-      };
-
-      pixmanSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/pixman/pixman/-/archive/9cc163c9da0fb4da430641715313d95a6ec466d9/pixman-9cc163c9da0fb4da430641715313d95a6ec466d9.tar.gz";
-        hash = "sha256-SiXzRtCuAkbg4LBFc3USTRwj9qsAtLyfzaDMed8h7Cc=";
-      };
-
-      xkbcompSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/app/xkbcomp/-/archive/e26102f28f08e5432b1ad44bbaef7f32aff199f6/xkbcomp-e26102f28f08e5432b1ad44bbaef7f32aff199f6.tar.gz";
-        hash = "sha256-CrQwo4CsmKu7TDgxCmxpFdfV7q4Ukeo1QhnVQpnJ9ug=";
-      };
-
-      xorgprotoSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/proto/xorgproto/-/archive/81931cc0fd4761b42603f7da7d4f50fc282cecc6/xorgproto-81931cc0fd4761b42603f7da7d4f50fc282cecc6.tar.gz";
-        hash = "sha256-qE8iVkkGBrbRDRBPruedESIEAaeooyxXtzWK3iwXgws=";
-      };
-
-      xserverSrc = fetchzip {
-        url = "https://gitlab.freedesktop.org/xorg/xserver/-/archive/2403cd5352b2a60d045b7f53c3c30002eb877f57/xserver-2403cd5352b2a60d045b7f53c3c30002eb877f57.tar.gz";
-        hash = "sha256-IZvwTVnzQamnk4W+OzY3ovHLs9Z0vhPAUXDQkeFwFew=";
-      };
-
-      src = runCommand "termux-x11-src-${shortRev}" { } ''
-        cp -R ${rootSrc} "$out"
-        chmod -R u+w "$out"
-
-        rm -rf \
-          "$out/app/src/main/cpp/bzip2" \
-          "$out/app/src/main/cpp/libepoxy" \
-          "$out/app/src/main/cpp/libfontenc" \
-          "$out/app/src/main/cpp/libtirpc" \
-          "$out/app/src/main/cpp/libx11" \
-          "$out/app/src/main/cpp/libxau" \
-          "$out/app/src/main/cpp/libxcvt" \
-          "$out/app/src/main/cpp/libxdmcp" \
-          "$out/app/src/main/cpp/libxfont" \
-          "$out/app/src/main/cpp/libxkbfile" \
-          "$out/app/src/main/cpp/libxshmfence" \
-          "$out/app/src/main/cpp/libxtrans" \
-          "$out/app/src/main/cpp/pixman" \
-          "$out/app/src/main/cpp/xkbcomp" \
-          "$out/app/src/main/cpp/xorgproto" \
-          "$out/app/src/main/cpp/xserver"
-
-        cp -R ${bzip2Src} "$out/app/src/main/cpp/bzip2"
-        cp -R ${libepoxySrc} "$out/app/src/main/cpp/libepoxy"
-        cp -R ${libfontencSrc} "$out/app/src/main/cpp/libfontenc"
-        cp -R ${libtirpcSrc} "$out/app/src/main/cpp/libtirpc"
-        cp -R ${libx11Src} "$out/app/src/main/cpp/libx11"
-        cp -R ${libxauSrc} "$out/app/src/main/cpp/libxau"
-        cp -R ${libxcvtSrc} "$out/app/src/main/cpp/libxcvt"
-        cp -R ${libxdmcpSrc} "$out/app/src/main/cpp/libxdmcp"
-        cp -R ${libxfontSrc} "$out/app/src/main/cpp/libxfont"
-        cp -R ${libxkbfileSrc} "$out/app/src/main/cpp/libxkbfile"
-        cp -R ${libxshmfenceSrc} "$out/app/src/main/cpp/libxshmfence"
-        cp -R ${libxtransSrc} "$out/app/src/main/cpp/libxtrans"
-        cp -R ${pixmanSrc} "$out/app/src/main/cpp/pixman"
-        cp -R ${xkbcompSrc} "$out/app/src/main/cpp/xkbcomp"
-        cp -R ${xorgprotoSrc} "$out/app/src/main/cpp/xorgproto"
-        cp -R ${xserverSrc} "$out/app/src/main/cpp/xserver"
-      '';
 
       androidSdk = androidSdkBuilder (s: [
         s.cmdline-tools-latest
