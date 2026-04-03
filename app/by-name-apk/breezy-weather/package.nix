ef1986e388c7b8eafab079ccd744032b5f4d9362
Mio <mio@mio19.uk>
2026-04-03 18:28:05 +1100

    save

--- app/by-name-apk/breezy-weather/package.nix
+++ app/by-name-apk/breezy-weather/package.nix
@@ -76,6 +76,19 @@ let
         echo "breezy.openweather.key=$(echo ZDljOTEwM2E3NGE0MzhlYWMwOTUyYTM0ZDFiNTgwZTYK | base64 -d)" >> local.properties
         echo "breezy.mf.key=$(echo X19XajdkVlNUalY5WUd1MWd1dmVMeURxMGc3UzdUZlRqYUhCVFBUcE8wa2o4X18K | base64 -d)" >> local.properties
         echo "breezy.mf.jwtKey=$(echo ODRhMzhkYjhjZGQ2NjJhMjY0MmY1MjZmMTE1ODhlN2UK | base64 -d)" >> local.properties
+        echo "breezy.accu.key=$(echo NDY2YTRhOTVlMmE5NDgzZThmM2ZjMjJkOWJiMjM5NWYK | base64 -d)" >> local.properties
+        echo "breezy.aemet.key=$(echo ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKemRXSWlPaUpoY0dsQVluSmxaWHA1ZDJWaGRHaGxjaTV2Y21jaUxDSnFkR2tpT2lJMlltUTFNVGxrWWkxbFkyVmlMVFExTVdVdE9EUXhNeTFrWkRjNU9EaGpaak14WlRVaUxDSnBjM01pT2lKQlJVMUZWQ0lzSW1saGRDSTZNVGN6TWpjeU5EVXdOeXdpZFhObGNrbGtJam9pTm1Ka05URTVaR0l0WldObFlpMDBOVEZsTFRnME1UTXRaR1EzT1RnNFkyWXpNV1UxSWl3aWNtOXNaU0k2SWlKOS5rUWtqMXRySmw3QkowNmtOdjZDSDN2LWpnWmRrVVVEa1Z5VHdISzBpMlhJCg== | base64 -d)" >> local.properties
+        echo "breezy.atmoaura.key=$(echo ZTEzOWVkNDFmODEyMzgzZWQ4ODY3OGY4ZTdmYTc0NGYK | base64 -d)" >> local.properties
+        echo "breezy.atmofrance.key=$(echo M2JlMmVjYjFhNThxNmd1eTY4NDVmNWo2OWE5ZDlkaDYK | base64 -d)" >> local.properties
+        echo "breezy.atmograndest.key=$(echo MXw4d3JJNTNPWlBkUUdvaGdtb0Jrd0o0U1ZWVlhzQklBYWpubE1RSXo4Cg== | base64 -d)" >> local.properties
+        echo "breezy.atmohdf.key=$(echo ZjE3MmQwZGEtZWFmMS00YTllLTg3YWYtYWEzYzZlM2U5YTcwCg== | base64 -d)" >> local.properties
+        echo "breezy.atmosud.key=$(echo OTM3MWU0Zjk1NTA5MzEwZjY2ZGJhMDg3ZjJmNDY1ZGMK | base64 -d)" >> local.properties
+        echo "breezy.baiduip.key=$(echo R00xZXZ1bG92R041RTQxcDZOQzcyTFczcWw1ZDBuTkcK | base64 -d)" >> local.properties
+        echo "breezy.bmkg.key=$(echo ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnBaQ0k2SWpGak5XRmtaV1V4WXpZNU16TTBOalkyTjJFelpXTTBNV1JsTWpCbVpXWmhORGN4T1ROall6Y3laRGd3TUdSaU4yWm1abUZsTVdWaFlqY3haR1l5WWpRaUxDSnBZWFFpT2pFM01ERTFPRE16TnpsOS5EMVZOcE1vVFVWRk9VdVFXMHkydlNqdHRad2owc0tCWDMzS3lya2FSTWNRCg== | base64 -d)" >> local.properties
+        echo "breezy.cwa.key=$(echo Q1dBLUM5ODdERkFBLTdEREUtNEU1OS1CMjgxLTVEQjBEOTgwRDI3MQo= | base64 -d)" >> local.properties
+        echo "breezy.eccc.key=$(echo YTNjNjE5MmEtMjA0OC00NTMyLThjYjgtZGY3Y2FkOTNmYmYyCg== | base64 -d)" >> local.properties
+        echo "breezy.geonames.key=$(echo YnJlZXp5d2VhdGhlcgo= | base64 -d)" >> local.properties
+        echo "breezy.metie.key=$(echo ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6STFOaUo5LmV5SnBjM01pT2lKdFpYUXRhbmQwSWl3aWMzVmlJam94TENKcFlYUWlPakUxTWpnNE1USTJNRFVzSW1WNGNDSTZOemd6TmpBeE1qWXdOWDAuTVpTQkRrU1JHMnVXZ1hfVVZLX1BhMk9mdzFMeGpVczF6Sks3TnkwM3NCUQo= | base64 -d)" >> local.properties
       '';
 
       preConfigure = ''
