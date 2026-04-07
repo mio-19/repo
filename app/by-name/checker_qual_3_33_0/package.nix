edb7d09bc2398a3c2574d2a9f9966878e3f4b390
Mio <mio@mio19.uk>
2026-04-08 09:43:15 +1000

    by-name: build more gradle libs from source

---
+++ app/by-name/checker_qual_3_33_0/package.nix
@@ -0,0 +1,61 @@
+{
+  fetchurl,
+  jdk21,
+  lib,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "checker-qual";
+  version = "3.33.0";
+
+  src = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/org/checkerframework/checker-qual/${finalAttrs.version}/checker-qual-${finalAttrs.version}-sources.jar";
+    hash = "sha256-RD+mFRmCu0xs5i4pOPU2YAhbE6fc61FyAnd7h9Deosc=";
+  };
+
+  pom = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/org/checkerframework/checker-qual/${finalAttrs.version}/checker-qual-${finalAttrs.version}.pom";
+    hash = "sha256-9VqSICenj92LPqFaDYv+P+xqXOrDDIaqivpKW5sN9gM=";
+  };
+
+  nativeBuildInputs = [ jdk21 ];
+
+  dontConfigure = true;
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    tmp="$(mktemp -d)"
+    trap 'rm -rf "$tmp"' EXIT
+    cd "$tmp"
+    ${jdk21}/bin/jar xf "$src"
+
+    mkdir -p classes
+    find . -name '*.java' | sort > sources.txt
+    ${jdk21}/bin/javac --release 8 -d classes @sources.txt
+
+    while IFS= read -r path; do
+      install -Dm644 "$path" "classes/$path"
+    done < <(find . -type f ! -name '*.java' ! -path './META-INF/maven/*' | sort)
+
+    (
+      cd classes
+      ${jdk21}/bin/jar cf "$tmp/checker-qual-${finalAttrs.version}.jar" .
+    )
+
+    mkdir -p "$out"
+    install -Dm644 "$tmp/checker-qual-${finalAttrs.version}.jar" "$out/checker-qual-${finalAttrs.version}.jar"
+    install -Dm644 "$pom" "$out/checker-qual-${finalAttrs.version}.pom"
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Checker Framework annotations";
+    homepage = "https://checkerframework.org/";
+    license = licenses.gpl2Only;
+    platforms = platforms.unix;
+  };
+})
--- app/by-name/gradle2nix_overrides/package.nix
+++ app/by-name/gradle2nix_overrides/package.nix
@@ -1,4 +1,8 @@
 {
+  checker_qual_3_33_0,
+  checker_qual_3_43_0,
+  commons_io_2_13_0,
+  commons_io_2_16_1,
   error_prone_annotations_2_27_0,
   error_prone_annotations_2_28_0,
   failureaccess_1_0_1,
@@ -8,6 +12,7 @@
   guava_33_3_1_jre,
   j2objc_annotations_2_8,
   j2objc_annotations_3_0_0,
+  jsr305_3_0_2,
   slf4j_api_2_0_17,
 }:
 
@@ -15,6 +20,18 @@
   "com.google.code.gson:gson:2.11.0" = {
     "gson-2.11.0.jar" = _: "${gson_2_11_0}/gson-2.11.0.jar";
     "gson-2.11.0.pom" = _: "${gson_2_11_0}/gson-2.11.0.pom";
+  };
+  "com.google.code.findbugs:jsr305:3.0.2" = {
+    "jsr305-3.0.2.jar" = _: "${jsr305_3_0_2}/jsr305-3.0.2.jar";
+    "jsr305-3.0.2.pom" = _: "${jsr305_3_0_2}/jsr305-3.0.2.pom";
+  };
+  "commons-io:commons-io:2.13.0" = {
+    "commons-io-2.13.0.jar" = _: "${commons_io_2_13_0}/commons-io-2.13.0.jar";
+    "commons-io-2.13.0.pom" = _: "${commons_io_2_13_0}/commons-io-2.13.0.pom";
+  };
+  "commons-io:commons-io:2.16.1" = {
+    "commons-io-2.16.1.jar" = _: "${commons_io_2_16_1}/commons-io-2.16.1.jar";
+    "commons-io-2.16.1.pom" = _: "${commons_io_2_16_1}/commons-io-2.16.1.pom";
   };
   "com.google.guava:guava:31.1-android" = {
     "guava-31.1-android.jar" = _: "${guava_31_1_android}/guava-31.1-android.jar";
@@ -66,6 +83,14 @@
   "com.google.j2objc:j2objc-annotations:3.0.0" = {
     "j2objc-annotations-3.0.0.jar" = _: "${j2objc_annotations_3_0_0}/j2objc-annotations-3.0.0.jar";
     "j2objc-annotations-3.0.0.pom" = _: "${j2objc_annotations_3_0_0}/j2objc-annotations-3.0.0.pom";
+  };
+  "org.checkerframework:checker-qual:3.33.0" = {
+    "checker-qual-3.33.0.jar" = _: "${checker_qual_3_33_0}/checker-qual-3.33.0.jar";
+    "checker-qual-3.33.0.pom" = _: "${checker_qual_3_33_0}/checker-qual-3.33.0.pom";
+  };
+  "org.checkerframework:checker-qual:3.43.0" = {
+    "checker-qual-3.43.0.jar" = _: "${checker_qual_3_43_0}/checker-qual-3.43.0.jar";
+    "checker-qual-3.43.0.pom" = _: "${checker_qual_3_43_0}/checker-qual-3.43.0.pom";
   };
   "org.slf4j:slf4j-api:2.0.17" = {
     "slf4j-api-2.0.17.jar" = _: "${slf4j_api_2_0_17}/slf4j-api-2.0.17.jar";
