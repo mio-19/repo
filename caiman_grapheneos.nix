4669f474b8bb183f737eb660e9d6573491a3e192
Mio <mio@mio19.uk>
2026-04-07 22:55:23 +1000

    gos_caimito_kernel.nix

--- caiman_grapheneos.nix
+++ caiman_grapheneos.nix
@@ -15,7 +15,7 @@ args@{
   source.dirs."device/google/caimito-kernels/6.1" = lib.mkForce {
     src =
       let
-        src = pkgs-unstable.callPackage ./grapheneos_caimito_kernel.nix {
+        src = pkgs-unstable.callPackage ./gos_caimito_kernel.nix {
           inherit (config) enableLindroid enableDroidspaces;
         };
       in
