a951a8ae3dba86081fbc725572468ecb4b05ec66
Mio <mio@mio19.uk>
2026-04-08 16:02:42 +1000

    app/recorder: convert to gradle2nix

--- app/by-name-apk/recorder/package.nix
+++ app/by-name-apk/recorder/package.nix
@@ -1,10 +1,11 @@
 {
   mk-apk-package,
+  gradle2nix_overrides,
+  gradle2nixBuilders,
   sources,
   lib,
   jdk21,
   gradle-packages,
-  stdenv,
   apksigner,
   writableTmpDirAsHomeHook,
   androidSdkBuilder,
@@ -15,79 +16,79 @@ let
     version
     ;
 
-  appPackage =
-    let
-      androidSdk = androidSdkBuilder (s: [
-        s.cmdline-tools-latest
-        s.platform-tools
-        s.platforms-android-36
-        s.build-tools-35-0-0
-      ]);
+  androidSdk = androidSdkBuilder (s: [
+    s.cmdline-tools-latest
+    s.platform-tools
+    s.platforms-android-36
+    s.build-tools-35-0-0
+  ]);
 
-      gradle =
-        (gradle-packages.mkGradle {
-          version = "9.1.0";
-          hash = "sha256-oX3dhaJran9d23H/iwX8UQTAICxuZHgkKXkMkzaGyAY=";
-          defaultJava = jdk21;
-        }).wrapped;
-    in
-    stdenv.mkDerivation (finalAttrs: {
-      pname = "recorder";
-      inherit version src;
+  gradle =
+    (gradle-packages.mkGradle {
+      version = "9.1.0";
+      hash = "sha256-oX3dhaJran9d23H/iwX8UQTAICxuZHgkKXkMkzaGyAY=";
+      defaultJava = jdk21;
+    }).wrapped;
 
-      gradleBuildTask = ":app:assembleRelease";
-      gradleUpdateTask = finalAttrs.gradleBuildTask;
+  appPackage = gradle2nixBuilders.buildGradlePackage rec {
+    pname = "recorder";
+    inherit version src gradle;
 
-      mitmCache = gradle.fetchDeps {
-        inherit (finalAttrs) pname;
-        pkg = finalAttrs.finalPackage;
-        data = ./recorder_deps.json;
-        silent = false;
-        useBwrap = false;
-      };
+    lockFile = ./gradle.lock;
+    overrides = gradle2nix_overrides;
+    buildJdk = jdk21;
 
-      nativeBuildInputs = [
-        gradle
-        jdk21
-        apksigner
-        writableTmpDirAsHomeHook
-      ];
+    nativeBuildInputs = [
+      androidSdk
+      gradle
+      jdk21
+      apksigner
+      writableTmpDirAsHomeHook
+    ];
 
-      env = {
-        JAVA_HOME = jdk21;
-        ANDROID_HOME = "${androidSdk}/share/android-sdk";
-        ANDROID_SDK_ROOT = "${androidSdk}/share/android-sdk";
-        ANDROID_AAPT2_FROM_MAVEN_OVERRIDE = "${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2";
-      };
+    dontUseGradleConfigure = true;
 
-      preConfigure = ''
-        export ANDROID_USER_HOME="$HOME/.android"
-        mkdir -p "$ANDROID_USER_HOME"
-        echo "sdk.dir=${androidSdk}/share/android-sdk" > local.properties
-      '';
+    env = {
+      ANDROID_HOME = "${androidSdk}/share/android-sdk";
+      ANDROID_SDK_ROOT = "${androidSdk}/share/android-sdk";
+      ANDROID_AAPT2_FROM_MAVEN_OVERRIDE = "${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2";
+    };
 
-      gradleFlags = [
-        "-xlintVitalRelease"
-        "-Dorg.gradle.java.installations.auto-download=false"
-        "-Dorg.gradle.java.installations.paths=${jdk21}"
-        "-Dandroid.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2"
-        "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2"
-      ];
+    preConfigure = ''
+      export ANDROID_USER_HOME="$HOME/.android"
+      export GRADLE_USER_HOME="$(mktemp -d)"
+      export TERM=dumb
+      mkdir -p "$ANDROID_USER_HOME"
+      echo "sdk.dir=${androidSdk}/share/android-sdk" > local.properties
+      gradleFlagsArray+=(--no-daemon --init-script "$gradleInitScript" --offline)
+    '';
 
-      installPhase = ''
-        runHook preInstall
-        apk_path="$(echo app/build/outputs/apk/release/*-release-unsigned.apk)"
-        install -Dm644 "$apk_path" "$out/recorder.apk"
-        runHook postInstall
-      '';
+    gradleFlags = [
+      "-x"
+      "lintVitalRelease"
+      "-Dorg.gradle.java.home=${jdk21.home}"
+      "-Dorg.gradle.java.installations.auto-download=false"
+      "-Dorg.gradle.java.installations.paths=${jdk21}"
+      "-Dandroid.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2"
+      "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/35.0.0/aapt2"
+    ];
 
-      meta = with lib; {
-        description = "LineageOS Recorder app";
-        homepage = "https://github.com/LineageOS/android_packages_apps_Recorder";
-        license = licenses.asl20;
-        platforms = platforms.unix;
-      };
-    });
+    gradleBuildFlags = ":app:assembleRelease";
+
+    installPhase = ''
+      runHook preInstall
+      apk_path="$(echo app/build/outputs/apk/release/*-release-unsigned.apk)"
+      install -Dm644 "$apk_path" "$out/recorder.apk"
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "LineageOS Recorder app";
+      homepage = "https://github.com/LineageOS/android_packages_apps_Recorder";
+      license = licenses.asl20;
+      platforms = platforms.unix;
+    };
+  };
 in
 mk-apk-package {
   inherit appPackage;
