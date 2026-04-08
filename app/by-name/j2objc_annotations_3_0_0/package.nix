8ab69f514e6b1d4e2737b8a0ae4fadf9636759a1
Mio <mio@mio19.uk>
2026-04-08 12:03:26 +1000

    j2objc revert

--- app/by-name/j2objc_annotations_3_0_0/package.nix
+++ app/by-name/j2objc_annotations_3_0_0/package.nix
@@ -1,5 +1,5 @@
 {
-  fetchFromGitHub,
+  fetchurl,
   jdk21,
   lib,
   stdenv,
@@ -9,11 +9,14 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "j2objc-annotations";
   version = "3.0.0";
 
-  src = fetchFromGitHub {
-    owner = "google";
-    repo = "j2objc";
-    tag = finalAttrs.version;
-    hash = "sha256-ljnnsyMhDVicgwsFPum5Wsv4fS47rsfTqBWhxYYib40=";
+  src = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/com/google/j2objc/j2objc-annotations/${finalAttrs.version}/j2objc-annotations-${finalAttrs.version}-sources.jar";
+    hash = "sha256-vWABmgQjw6Al72qyT+B2H19F/7SKjMp0oBtnjeEQXTg=";
+  };
+
+  pom = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/com/google/j2objc/j2objc-annotations/${finalAttrs.version}/j2objc-annotations-${finalAttrs.version}.pom";
+    hash = "sha256-I7PQOeForYndEUaY5t1744P0osV3uId9gsc6ZRXnShc=";
   };
 
   nativeBuildInputs = [ jdk21 ];
@@ -27,8 +30,9 @@ stdenv.mkDerivation (finalAttrs: {
     tmp="$(mktemp -d)"
     trap 'rm -rf "$tmp"' EXIT
     cd "$tmp"
+    ${jdk21}/bin/jar xf "$src"
     mkdir -p classes
-    find "${finalAttrs.src}/annotations/src/main/java" -name '*.java' ! -name 'module-info.java' | sort > sources.txt
+    find . -name '*.java' ! -name 'module-info.java' | sort > sources.txt
     ${jdk21}/bin/javac --release 8 -d classes @sources.txt
     (
       cd classes
@@ -37,7 +41,7 @@ stdenv.mkDerivation (finalAttrs: {
 
     mkdir -p "$out"
     install -Dm644 "$tmp/j2objc-annotations-${finalAttrs.version}.jar" "$out/j2objc-annotations-${finalAttrs.version}.jar"
-    install -Dm644 "${finalAttrs.src}/annotations/pom.xml" "$out/j2objc-annotations-${finalAttrs.version}.pom"
+    install -Dm644 "$pom" "$out/j2objc-annotations-${finalAttrs.version}.pom"
 
     runHook postInstall
   '';
