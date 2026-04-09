cb2fc4428bdb6f824b65685a6add613ca5132c7a
Mio <mio@mio19.uk>
2026-04-09 22:29:06 +1000

    13 -> gradle_8_14_M8

--- app/by-name-lib/gradle_8_14_M8/package.nix
+++ app/by-name-lib/gradle_8_14_M8/package.nix
@@ -1,13 +1,16 @@
 {
-  jdk11_headless,
+  jdk17_headless,
   jdk21_headless,
-  gradle-packages,
+  gradle_8_13,
   gradle-from-source,
 }:
-(gradle-packages.mkGradle {
-  version = "8.14-milestone-8";
-  hash = "sha256-a242N0+A0+VZTZ/acEC/ReHkYX7LlVKfT1yud9TGzY8=";
+gradle-from-source {
+  version = "8.14.0-M8";
+  hash = "sha256-uP5XNVZO/gW3NQpIv3EMXlIWI8OCY8qSJWIgTysLl0k=";
+  lockFile = ./gradle.lock;
   defaultJava = jdk21_headless;
-}).wrapped
-# nix-shell -p jdk17
-# nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.13
+  buildJdk = jdk17_headless;
+  # nix-shell -p jdk17
+  # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.13
+  bootstrapGradle = gradle_8_13;
+}
