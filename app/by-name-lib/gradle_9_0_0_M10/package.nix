9f51598a2b6919668c538c8e283ddd2f7b234e80
Mio <mio@mio19.uk>
2026-04-09 20:50:11 +1000

    gradle_9_0_0_M10

---
+++ app/by-name-lib/gradle_9_0_0_M10/package.nix
@@ -0,0 +1,15 @@
+{
+  jdk17_headless,
+  jdk21_headless,
+  gradle-from-source,
+  gradle_9_0_0_M4,
+}:
+gradle-from-source {
+  version = "9.0.0-M10";
+  hash = "";
+  lockFile = ./gradle.lock;
+  defaultJava = jdk21_headless;
+  buildJdk = jdk17_headless;
+  # nix run github:tadfisher/gradle2nix/v2 -- --gradle-dist=https://services.gradle.org/distributions/gradle-9.0-milestone-4-bin.zip
+  bootstrapGradle = gradle_9_0_0_M4;
+}
--- app/by-name-lib/gradle_9_1_0/package.nix
+++ app/by-name-lib/gradle_9_1_0/package.nix
@@ -2,7 +2,7 @@
   jdk17_headless,
   jdk21_headless,
   gradle-from-source,
-  gradle_9_0_0_M1,
+  gradle_9_0_0_M10,
 }:
 gradle-from-source {
   version = "9.1.0";
@@ -11,5 +11,5 @@ gradle-from-source {
   defaultJava = jdk21_headless;
   buildJdk = jdk17_headless;
   # nix run github:tadfisher/gradle2nix/v2 -- --gradle-dist=https://services.gradle.org/distributions/gradle-9.0.0-milestone-10-bin.zip
-  bootstrapGradle = gradle_9_0_0_M1;
+  bootstrapGradle = gradle_9_0_0_M10;
 }
