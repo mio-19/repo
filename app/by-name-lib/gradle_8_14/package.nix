473e8452ec0bceb8bfa47e7f8c334f694a71f67c
Mio <mio@mio19.uk>
2026-04-09 21:07:21 +1000

    9.0.0-M7

--- app/by-name-lib/gradle_8_14/package.nix
+++ app/by-name-lib/gradle_8_14/package.nix
@@ -9,3 +9,5 @@
   hash = "sha256-Ya0xDTx9Pl2hMbdrvyK1pMB4bp2JLa6MFljUtITePKo=";
   defaultJava = jdk21_headless;
 }).wrapped
+
+# nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.13
