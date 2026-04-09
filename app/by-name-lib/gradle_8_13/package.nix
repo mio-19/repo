4433d7e820f6aab3a5f193338b295cfb297237b2
Mio <mio@mio19.uk>
2026-04-09 22:21:05 +1000

    # this version specifically ask for termurin branded jdk.

--- app/by-name-lib/gradle_8_13/package.nix
+++ app/by-name-lib/gradle_8_13/package.nix
@@ -1,5 +1,7 @@
 {
-  javaPackages,
+  temurin-bin-8,
+  temurin-bin-11,
+  temurin-bin-17,
   jdk21_headless,
   gradle_8_13_rc1,
   gradle-from-source,
@@ -9,11 +11,12 @@ gradle-from-source {
   hash = "sha256-VhV58GDtZRjlQFtG1knTbm7vJP2JrrSr5yD/3/+yTnM=";
   lockFile = ./gradle.lock;
   defaultJava = jdk21_headless;
-  buildJdk = javaPackages.compiler.openjdk17-bootstrap;
+  # this version specifically ask for termurin branded jdk.
+  buildJdk = temurin-bin-17;
   javaToolchains = [
-    javaPackages.compiler.openjdk8-bootstrap
-    javaPackages.compiler.openjdk11-bootstrap
-    javaPackages.compiler.openjdk17-bootstrap
+    temurin-bin-8
+    temurin-bin-11
+    temurin-bin-17
   ];
   # nix-shell -p javaPackages.compiler.openjdk17-bootstrap
   # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.13-rc-1
