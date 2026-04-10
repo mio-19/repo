7a0987b05f82fa7afcd3138b5315ceb248086de6
Mio <mio@mio19.uk>
2026-04-10 22:49:09 +1000

    gradle_8_6_rc2_fromsrc

--- app/lib/gradle_8_5_rc1/package.nix
+++ app/lib/gradle_8_5_rc1/package.nix
@@ -3,7 +3,7 @@
   gradle-packages,
 }:
 (gradle-packages.mkGradle {
-  version = "8.5";
-  hash = "";
+  version = "8.5-rc-1";
+  hash = "sha256-jHRGKx2D+LF8SDjJJfxMRtH7tEZ7GLihf1zaruRbfwk=";
   defaultJava = jdk17_headless;
 }).wrapped
--- app/lib/gradle_8_6_rc2/package.nix
+++ app/lib/gradle_8_6_rc2/package.nix
@@ -10,4 +10,4 @@
 }).wrapped
 
 # nix-shell -p javaPackages.compiler.openjdk11-bootstrap
-# nix run github:tadfisher/gradle2nix/v2  -- --gradle-wrapper=8.5
+# nix run github:tadfisher/gradle2nix/v2  -- --gradle-wrapper=8.5-rc-1
