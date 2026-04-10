3e065ace6d164a22bce39d6179fd8722d6a439cc
Mio <mio@mio19.uk>
2026-04-10 22:51:36 +1000

    save

--- app/lib/gradle_8_5_rc1/package.nix
+++ app/lib/gradle_8_5_rc1/package.nix
@@ -2,8 +2,12 @@
   jdk17_headless,
   gradle-packages,
 }:
+# 8.5.0-RC1
 (gradle-packages.mkGradle {
   version = "8.5-rc-1";
   hash = "sha256-jHRGKx2D+LF8SDjJJfxMRtH7tEZ7GLihf1zaruRbfwk=";
   defaultJava = jdk17_headless;
 }).wrapped
+
+# nix-shell -p javaPackages.compiler.openjdk11-bootstrap
+# nix run github:tadfisher/gradle2nix/v2  -- --gradle-wrapper=8.4
