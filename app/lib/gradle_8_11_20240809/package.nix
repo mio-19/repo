33a35b6a3874661dabf61407ccab9629a63beb58
Mio <mio@mio19.uk>
2026-04-10 11:46:03 +1000

    nix run github:tadfisher/gradle2nix/v2  -- --gradle-home=/nix/store/7mcaacs8xrp4n7nglp870b2rb3jh8sx5-gradle-8.11-20240807/libexec/gradle

--- app/lib/gradle_8_11_20240809/package.nix
+++ app/lib/gradle_8_11_20240809/package.nix
@@ -29,5 +29,7 @@ gradle-from-source {
     temurin-bin-11
     temurin-bin-17
   ];
+  # nix-shell -p javaPackages.compiler.openjdk17-bootstrap
+  # nix run github:tadfisher/gradle2nix/v2  -- --gradle-home=/nix/store/7mcaacs8xrp4n7nglp870b2rb3jh8sx5-gradle-8.11-20240807/libexec/gradle
   bootstrapGradle = gradle_8_11_20240807;
 }
