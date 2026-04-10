06631648f9b3b1b50b09fb416e6a753f5c094c98
Mio <mio@mio19.uk>
2026-04-10 23:49:53 +1000

    gradle_8_11_M1

--- app/lib/gradle_8_11_M1/package.nix
+++ app/lib/gradle_8_11_M1/package.nix
@@ -5,16 +5,17 @@
   jdk21_headless,
   gradle_8_11_20240920_2,
   gradle-from-source,
-  runCommand,
-  jq,
-  lib,
+  mergeLock,
 }:
 gradle-from-source {
   version = "8.11.0-M1";
   hash = "sha256-+0ijcrkUCFa1JTNM0rEEXgYuz0sZfHBdR4eWl5H2iDg=";
-  lockFile = runCommand "merged-lock" { } ''
-    ${lib.getExe jq} -s '.[0] * .[1]' ${gradle_8_11_20240920_2.unwrapped.passthru.lockFile} ${./more.gradle.lock} > $out
-  '';
+  lockFile = mergeLock [
+    gradle_8_11_20240920_2.unwrapped.passthru.lockFile
+    # org.gradle.buildtool.internal:configuration-cache-report:1.21
+    ../gradle_8_12_20241015/gradle.lock
+    ./more.gradle.lock
+  ];
   defaultJava = jdk21_headless;
   # this version specifically ask for termurin branded jdk.
   buildJdk = temurin-bin-11;
