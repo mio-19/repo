a823673b108b5aee29f265522527e811e1753769
Mio <mio@mio19.uk>
2026-04-10 15:40:34 +1000

    save

--- app/lib/gradle_8_10_2/package.nix
+++ app/lib/gradle_8_10_2/package.nix
@@ -3,13 +3,13 @@
   temurin-bin-11,
   temurin-bin-17,
   jdk21_headless,
-  gradle_8_12_rc1,
+  gradle_8_10_rc1,
   gradle-from-source,
   runCommand,
   jq,
 }:
 gradle-from-source {
-  version = "8.12";
+  version = "8.10.2";
   hash = "";
   lockFile =
     runCommand "merged-lock"
@@ -17,17 +17,17 @@ gradle-from-source {
         nativeBuildInputs = [ jq ];
       }
       ''
-        jq -s '.[0] * .[1]' ${../gradle_8_12_1/gradle.lock} ${./more.gradle.lock} > $out
+        jq -s '.[0] * .[1]' ${../gradle_8_10/gradle.lock} ${./more.gradle.lock} > $out
       '';
   defaultJava = jdk21_headless;
   # this version specifically ask for termurin branded jdk.
-  buildJdk = temurin-bin-17;
+  buildJdk = temurin-bin-11;
   javaToolchains = [
     temurin-bin-8
     temurin-bin-11
     temurin-bin-17
   ];
-  # nix-shell -p javaPackages.compiler.openjdk17-bootstrap
-  # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.12-rc-1
-  bootstrapGradle = gradle_8_12_rc1;
+  # nix-shell -p javaPackages.compiler.openjdk11-bootstrap
+  # nix run github:tadfisher/gradle2nix/v2  -- --gradle-wrapper=8.10-rc-1
+  bootstrapGradle = gradle_8_10_rc1;
 }
