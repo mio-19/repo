526a26161390337bd6b56e46d40ca0ad4d8b19c1
Mio <mio@mio19.uk>
2026-04-10 13:04:32 +1000

    save

--- app/lib/gradle_8_11_20240809/package.nix
+++ app/lib/gradle_8_11_20240809/package.nix
@@ -13,7 +13,7 @@ gradle-from-source {
   version = "8.11-20240809";
   rev = "d40cb09ed3c2f557ee731dd88dde0cae2f3f0ce1";
   hash = "sha256-xt5RLWbM1YAgg0IAl7OttqV1qjxpWzyRmQdGKjTXmr0=";
-  # TODO: org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:5.1.0 org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.10
+  # more.gradle.lock: org.gradle.kotlin.kotlin-dsl:org.gradle.kotlin.kotlin-dsl.gradle.plugin:5.1.0 org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.10 org.jetbrains.kotlin:kotlin-util-io:2.0.10
   # ./refresh-hashes.sh more.gradle.lock
   lockFile =
     runCommand "merged-lock"
@@ -34,7 +34,5 @@ gradle-from-source {
     temurin-bin-11
     temurin-bin-17
   ];
-  # nix-shell -p javaPackages.compiler.openjdk17-bootstrap
-  # nix run github:tadfisher/gradle2nix/v2  -- --gradle-home=/nix/store/7mcaacs8xrp4n7nglp870b2rb3jh8sx5-gradle-8.11-20240807/libexec/gradle
   bootstrapGradle = gradle_8_11_20240807;
 }
