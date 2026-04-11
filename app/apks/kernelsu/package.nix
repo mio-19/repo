6c09838e69f1532b94a68b2caf6c310cab38cae7
Mio <mio@mio19.uk>
2026-04-11 15:58:24 +1000

    "https://github.com/${kernelsuSrc.owner}/${kernelsuSrc.repo}/raw/refs/tags/${kernelsuSrc.tag}/userspace/ksud/Cargo.lock";

--- app/apks/kernelsu/package.nix
+++ app/apks/kernelsu/package.nix
@@ -84,7 +84,7 @@ let
             #lockFile = "${kernelsuSrc}/userspace/ksud/Cargo.lock";
             # this only fetches one file during nix evaluation
             lockFile = fetchurl {
-              url = "https://github.com/tiann/KernelSU/raw/refs/tags/v${version}/userspace/ksud/Cargo.lock";
+              url = "https://github.com/${kernelsuSrc.owner}/${kernelsuSrc.repo}/raw/refs/tags/${kernelsuSrc.tag}/userspace/ksud/Cargo.lock";
               hash = "sha256-fmsvdykF2oSNcjzTOQO+qFvsncMRnSMOwGhoCEZtvzs=";
             };
             outputHashes = {
