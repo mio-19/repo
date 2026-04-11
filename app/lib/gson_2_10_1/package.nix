a12b576509ee05cac13b76f17345a8237a28ffd8
Mio <mio@mio19.uk>
2026-04-11 22:59:39 +1000

    save

--- app/lib/gson_2_10_1/package.nix
+++ app/lib/gson_2_10_1/package.nix
@@ -1,5 +1,5 @@
 { gson_common }:
-gson_common rec {
+gson_common {
   version = "2.10.1";
   hash = "sha256-Hjex840nPoJ99l41VeMa9Eiq81QZOEYB2MGvdzQwMus=";
 }
--- app/lib/gson_2_8_6/package.nix
+++ app/lib/gson_2_8_6/package.nix
@@ -1,6 +1,5 @@
 { gson_common }:
-gson_common rec {
+gson_common {
   version = "2.8.6";
-  tag = "gson-parent-${version}";
   hash = "sha256-Y96Xx01C7t2vrM/WUgiu9tG5Lst2fhrgBatBFve4ZU4=";
 }
