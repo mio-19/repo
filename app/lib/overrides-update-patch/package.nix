42f17ef018250ffec2169ad960a06f4b4b452c71
Mio <mio@mio19.uk>
2026-04-11 20:19:24 +1000

    commons-codec:commons-codec:1.0~1.12 -> 1.13

--- app/lib/overrides-update-patch/package.nix
+++ app/lib/overrides-update-patch/package.nix
@@ -19,9 +19,36 @@
   };
 
   # WS-2019-0379 https://github.com/apache/incubator-kie-issues/issues/1785 Apache commons-codec before version “commons-codec-1.13-RC1” is vulnerable to information disclosure due to Improper Input validation.
+  "commons-codec:commons-codec:1.0" = {
+    "commons-codec-1.0.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.1" = {
+    "commons-codec-1.1.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.2" = {
+    "commons-codec-1.2.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.3" = {
+    "commons-codec-1.3.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.4" = {
+    "commons-codec-1.4.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.5" = {
+    "commons-codec-1.5.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
   "commons-codec:commons-codec:1.6" = {
     "commons-codec-1.6.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
     "commons-codec-1.6.pom" = _: "${commons_codec_1_6}/commons-codec-1.6.pom";
+  };
+  "commons-codec:commons-codec:1.7" = {
+    "commons-codec-1.7.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.8" = {
+    "commons-codec-1.8.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
+  };
+  "commons-codec:commons-codec:1.9" = {
+    "commons-codec-1.9.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
   };
   "commons-codec:commons-codec:1.10" = {
     "commons-codec-1.10.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
@@ -30,5 +57,8 @@
   "commons-codec:commons-codec:1.11" = {
     "commons-codec-1.11.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
     "commons-codec-1.11.pom" = _: "${commons_codec_1_11}/commons-codec-1.11.pom";
+  };
+  "commons-codec:commons-codec:1.12" = {
+    "commons-codec-1.12.jar" = _: "${commons_codec_1_13}/commons-codec-1.13.jar";
   };
 }
