679b5ba3fd98cefeac5e11d50495dd5ec92dea6a
Mio <mio@mio19.uk>
2026-04-12 21:24:17 +1000

    fixup

--- app/by-name/overrides-fromsrc-bare/utils.nix
+++ app/by-name/overrides-fromsrc-bare/utils.nix
@@ -29,9 +29,10 @@ rec {
           touch $out
         '';
       inherit (lib) mapAttrs' nameValuePair mapAttrs;
+      # TODO: don't add .asc for entries that already have an .asc
       entryNoAsc = entry: mapAttrs' (name: _: nameValuePair (name + ".asc") emptyAsc) entry;
     in
-    deepMergeUnique basic (mapAttrs (_: entryNoAsc) basic);
+    deepMerge (mapAttrs (_: entryNoAsc) basic) basic;
   # based on https://discourse.nixos.org/t/nix-function-to-merge-attributes-records-recursively-and-concatenate-arrays/2030/9
   deepMergeUnique =
     lhs: rhs:
@@ -49,6 +50,6 @@ rec {
       else if lValue == null then
         rValue
       else
-        throw "Conflict at ${rName}: ${lValue} vs ${rValue}"
+        throw "Conflict at ${rName}"
     ) rhs);
 }
