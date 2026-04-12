bec99c7754d6d75ad858614d9bb54737ab4bcea5
Mio <mio@mio19.uk>
2026-04-12 11:59:36 +1000

    libs-from-source

---
+++ app/lib/asm_common/package.nix
@@ -0,0 +1,67 @@
+{
+  fetchurl,
+  jdk25_headless,
+  lib,
+  stdenv,
+}:
+{
+  artifactId,
+  version,
+  srcHash,
+  pomHash,
+  classpath ? [ ],
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = artifactId;
+  inherit version;
+
+  src = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/org/ow2/asm/${artifactId}/${finalAttrs.version}/${artifactId}-${finalAttrs.version}-sources.jar";
+    hash = srcHash;
+  };
+
+  pom = fetchurl {
+    url = "https://repo.maven.apache.org/maven2/org/ow2/asm/${artifactId}/${finalAttrs.version}/${artifactId}-${finalAttrs.version}.pom";
+    hash = pomHash;
+  };
+
+  nativeBuildInputs = [ jdk25_headless ];
+
+  dontConfigure = true;
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    tmp="$(mktemp -d)"
+    trap 'rm -rf "$tmp"' EXIT
+    cd "$tmp"
+    jar xf "$src"
+
+    mkdir -p classes
+    find org -name '*.java' | sort > sources.txt
+    javac --release 8 \
+      ${lib.optionalString (classpath != [ ]) "-cp ${lib.concatStringsSep ":" classpath}"} \
+      -d classes \
+      @sources.txt
+
+    (
+      cd classes
+      jar cf "$tmp/${artifactId}-${finalAttrs.version}.jar" .
+    )
+
+    mkdir -p "$out"
+    install -Dm644 "$tmp/${artifactId}-${finalAttrs.version}.jar" "$out/${artifactId}-${finalAttrs.version}.jar"
+    install -Dm644 "$pom" "$out/${artifactId}-${finalAttrs.version}.pom"
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Java bytecode manipulation framework";
+    homepage = "https://asm.ow2.io/";
+    license = licenses.bsd3;
+    platforms = platforms.unix;
+  };
+})
---
+++ app/lib/asm_tree_9_9/package.nix
@@ -0,0 +1,11 @@
+{
+  asm_9_9,
+  asm_common,
+}:
+asm_common {
+  artifactId = "asm-tree";
+  version = "9.9";
+  srcHash = "sha256-nR/iYfodKZBMqdvHaHg5bna8IlGRZ2qMFq0maaIFMho=";
+  pomHash = "sha256-0BeI7Y5ujiSsr2y0p73MJzNBYlL6oAIylZ4+Lp3xv0Q=";
+  classpath = [ "${asm_9_9}/asm-9.9.jar" ];
+}
