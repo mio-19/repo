a9466d6d8a86afb9a7332fb041a3cb9e9b748d63
Mio <mio@mio19.uk>
2026-04-12 14:57:25 +1000

    remove vendor = JvmVendorSpec.ADOPTIUM?

--- app/lib/gradle_8_13/package.nix
+++ app/lib/gradle_8_13/package.nix
@@ -31,4 +31,23 @@ else
     # nix-shell -p javaPackages.compiler.openjdk17-bootstrap
     # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.13-rc-1
     bootstrapGradle = gradle_8_13_rc1;
+    postPatch =
+      let
+        # grep -rl 'vendor = JvmVendorSpec.ADOPTIUM' . | sed 's/.*/"&"/'
+        files = [
+          "./build-logic-settings/configuration-cache-compatibility/build.gradle.kts"
+          "./build-logic-settings/build-environment/build.gradle.kts"
+          "./build-logic/build-update-utils/src/main/kotlin/gradlebuild.update-versions.gradle.kts"
+          "./build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts"
+          "./platforms/jvm/language-java/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy"
+          "./platforms/documentation/docs/src/snippets/java/toolchain-filters/kotlin/build.gradle.kts"
+          "./platforms/documentation/docs/src/snippets/java/toolchain-filters/groovy/build.gradle"
+          "./build-logic-commons/settings.gradle.kts"
+          "./build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild/commons/JavaPluginExtensions.kt"
+        ];
+      in
+      ''
+        substituteInPlace ${builtins.concatStringsSep " " files} \
+          --replace-fail 'vendor = JvmVendorSpec.ADOPTIUM' ""
+      '';
   }
