a91839c9209a0396f71ff65874cdb77c223c7149
Mio <mio@mio19.uk>
2026-04-13 20:19:33 +1000

    save

--- libs/maven_3_9_14/package.nix
+++ libs/maven_3_9_14/package.nix
@@ -23,14 +23,13 @@ maven_nixpkgs.overrideAttrs (
     # https://maven.apache.org/guides/development/guide-building-maven.html
     buildPhase = ''
       runHook preBuild
-      mkdir out
       cp -r ${finalAttrs.mavenRepository} m2-repo
       chmod -R a+w m2-repo
       mvn -DdistributionTargetDir="out/apache-maven-${finalAttrs.version}" -Dmaven.repo.local=m2-repo -DskipITs -Dcpd.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true -DskipTests -Dmaven.test.skip=true -Dspotless.apply.skip=true -Dspotless.check.skip=true -Drat.skip=true -Denforcer.skip=true install
       runHook postBuild
     '';
     preInstall = ''
-      cd out
+      cd apache-maven/out
     '';
     bootstrapMaven = maven_nixpkgs;
     nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ finalAttrs.bootstrapMaven ];
