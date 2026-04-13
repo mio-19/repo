23b19f510ce3f1ccddb35bbe1e1e0ebe315d1f82
Mio <mio@mio19.uk>
2026-04-14 09:39:49 +1000

    save

--- libs/groovy_1_8_9/package.nix
+++ libs/groovy_1_8_9/package.nix
@@ -19,9 +19,6 @@ let
   bnd_401 = runCommand "bnd-0.0.401.pom" {} ''
       substitute ${bnd_384} $out --replace-fail '0.0.384' '0.0.401'
     '';
-  bnd_402 = runCommand "bnd-0.0.402.pom" {} ''
-      substitute ${bnd_384} $out --replace-fail '0.0.384' '0.0.402'
-    '';
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "groovy";
@@ -33,29 +30,22 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-pG9jsyMEUMVoeqnI04Tk5g0Y5VRxBcTxVSw4HyGqF0E=";
   };
   sourceRoot = finalAttrs.src.name;
-  buildPhase = ''
-    runHook preBuild
-    mkdir out
-    cp -r ${finalAttrs.mavenRepository} m2-repo
-    chmod -R a+w m2-repo
-    ln -s ${bnd_401} m2-repo/biz/aQute/bnd/0.0.401/bnd-0.0.401.pom
-    if [ ! -f ${finalAttrs.mavenRepository}/biz/aQute/bnd/0.0.401/bnd-0.0.401.jar ]; then
-      echo "Error: ${finalAttrs.mavenRepository}/biz/aQute/bnd/0.0.401/bnd-0.0.401.jar not found"
-      exit 1
-    fi
-    mkdir m2-repo/biz/aQute/bnd/0.0.402
-    ln -s ${bnd_402} m2-repo/biz/aQute/bnd/0.0.402/bnd-0.0.402.pom
-    ln -s ${finalAttrs.mavenRepository}/biz/aQute/bnd/0.0.401/bnd-0.0.401.jar m2-repo/biz/aQute/bnd/0.0.402/bnd-0.0.402.jar
-    ant -Dmaven.repo.local=$PWD/m2-repo install -DskipTests=true -DskipOsgi=true
-    runHook postBuild
-  '';
   patches = [
     ./a.patch
   ];
   postPatch = ''
     sed -i 's|<contains string="''${ant.version}" substring="1.1"></contains>||g' build.xml
     sed -i 's|\bParameter\b|org.codehaus.groovy.ast.Parameter|g' src/main/org/codehaus/groovy/vmplugin/v5/Java5.java
-    substituteInPlace build.gradle config/maven/groovy-tools.pom --replace-fail '0.0.401' '0.0.402'
+    substituteInPlace build.xml --replace-fail '<target name="stagedcompile" depends="-init,ensureGrammars,-jvm14BuildWarning,exceptionUtils" unless="uber">' '<target name="stagedcompile" depends="-init,-jvm14BuildWarning,exceptionUtils" unless="uber">'
+  '';
+  buildPhase = ''
+    runHook preBuild
+    mkdir out
+    cp -r ${finalAttrs.mavenRepository} m2-repo
+    chmod -R a+w m2-repo
+    ln -s ${bnd_401} m2-repo/biz/aQute/bnd/0.0.401/bnd-0.0.401.pom
+    ant -Dmaven.repo.local=$PWD/m2-repo install -DskipTests=true -DskipOsgi=true
+    runHook postBuild
   '';
   nativeBuildInputs = [
     ant
