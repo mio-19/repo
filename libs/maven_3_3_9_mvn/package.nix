2802340720282993feaf85419fa6c7b38500d5ed
Mio <mio@mio19.uk>
2026-04-13 22:31:16 +1000

    save

---
+++ libs/maven_3_3_9_mvn/package.nix
@@ -0,0 +1,78 @@
+{
+  callPackage,
+  fetchFromGitHub,
+  buildMavenRepositoryFromLockFile,
+  buildMavenRepository,
+  libsUtils,
+  jdk8_headless,
+  lib,
+}:
+let
+  maven_nixpkgs = callPackage ./nixpkgs.nix { };
+  inherit (libsUtils) checkMavenProvides exposeMavenProvides;
+  inherit (buildMavenRepositoryFromLockFile.passthru) mergeDeps readDeps;
+in
+maven_nixpkgs.overrideAttrs (
+  finalAttrs: prevAttrs:
+  let
+    useDistributionTargetDir = lib.strings.compareVersions finalAttrs.version "3.3.9" > 0;
+    extraFlags =
+      lib.optionals (lib.strings.compareVersions finalAttrs.version "3.3.9" <= 0) [
+        "-Dremoteresources.skip=true"
+      ]
+      ++ lib.optionals useDistributionTargetDir [
+        ''-DdistributionTargetDir="out/apache-maven-${finalAttrs.version}"''
+      ];
+  in
+  {
+    version = "3.3.9";
+    src = fetchFromGitHub {
+      owner = "apache";
+      repo = "maven";
+      tag = "maven-${finalAttrs.version}";
+      hash = "sha256-qqk0FyPo0X43d9Co7qe193D9lIj6DbJ7Tu7WGoD5QkY=";
+    };
+    sourceRoot = finalAttrs.src.name;
+    # https://maven.apache.org/guides/development/guide-building-maven.html
+    buildPhase = ''
+      runHook preBuild
+      cp -r ${finalAttrs.mavenRepository} m2-repo
+      chmod -R a+w m2-repo
+      mvn --offline  -Dmaven.repo.local=m2-repo -DskipITs -Dcpd.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true -DskipTests -Dmaven.test.skip=true -Dspotless.apply.skip=true -Dspotless.check.skip=true -Drat.skip=true -Denforcer.skip=true ${builtins.concatStringsSep " " extraFlags} install
+    '';
+    preInstall =
+      lib.optionalString (!useDistributionTargetDir) ''
+        mkdir apache-maven/out
+        cd apache-maven/out
+        tar -xzf ../target/apache-maven-${finalAttrs.version}-bin.tar.gz
+        cd ../..
+      ''
+      # After deleting symlinks, left are what it published.
+      + ''
+        find m2-repo -type l -delete
+        for i in {1..10}; do find m2-repo -type d -empty -delete; done
+        mv m2-repo apache-maven/out/apache-maven-${finalAttrs.version}/
+        runHook postBuild
+        cd apache-maven/out
+      '';
+    bootstrapMaven = maven_nixpkgs;
+    jdk = jdk8_headless;
+    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [
+      finalAttrs.bootstrapMaven
+      finalAttrs.jdk
+    ];
+    mavenRepository = buildMavenRepository { dependencies = readDeps finalAttrs.passthru.mavenDeps; };
+    doInstallCheck = true;
+    installCheckPhase = checkMavenProvides finalAttrs;
+    passthru = prevAttrs.passthru // {
+      # also run jq -S '.' on it.
+      # also need to manually remove 3.3.9 entries from linux-m2.json
+      mavenDeps = ./linux-m2.json;
+    };
+    meta = prevAttrs.meta // {
+      mavenProvides = exposeMavenProvides finalAttrs;
+      # TODO
+      mavenProvidesInternal = { };
+    };
+  }
+)
--- libs/maven_3_9_14/package.nix
+++ libs/maven_3_9_14/package.nix
@@ -1,16 +1,13 @@
 {
-  callPackage,
+  maven_3_3_9_mvn,
   fetchFromGitHub,
   buildMavenRepositoryFromLockFile,
-  buildMavenRepository,
-  libsUtils,
+  jdk11_headless,
 }:
 let
-  maven_nixpkgs = callPackage ./nixpkgs.nix { };
-  inherit (libsUtils) checkMavenProvides exposeMavenProvides;
   inherit (buildMavenRepositoryFromLockFile.passthru) mergeDeps fromGradleLock;
 in
-maven_nixpkgs.overrideAttrs (
+maven_3_3_9_mvn.overrideAttrs (
   finalAttrs: prevAttrs: {
     version = "3.9.14";
     src = fetchFromGitHub {
@@ -19,27 +16,8 @@ maven_nixpkgs.overrideAttrs (
       tag = "maven-${finalAttrs.version}";
       hash = "sha256-fCqLWXxCznnD8bzHHaBWD7r0yb3mKu+5ApxpqYP42tg=";
     };
-    sourceRoot = finalAttrs.src.name;
-    # https://maven.apache.org/guides/development/guide-building-maven.html
-    buildPhase = ''
-      runHook preBuild
-      cp -r ${finalAttrs.mavenRepository} m2-repo
-      chmod -R a+w m2-repo
-      mvn --offline -DdistributionTargetDir="out/apache-maven-${finalAttrs.version}" -Dmaven.repo.local=m2-repo -DskipITs -Dcpd.skip=true -Dpmd.skip=true -Dcheckstyle.skip=true -DskipTests -Dmaven.test.skip=true -Dspotless.apply.skip=true -Dspotless.check.skip=true -Drat.skip=true -Denforcer.skip=true install
-    '';
-    # After deleting symlinks, left are what it published.
-    preInstall = ''
-      find m2-repo -type l -delete
-      for i in {1..10}; do find m2-repo -type d -empty -delete; done
-      mv m2-repo apache-maven/out/apache-maven-${finalAttrs.version}/
-      runHook postBuild
-      cd apache-maven/out
-    '';
-    bootstrapMaven = maven_nixpkgs;
-    nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ finalAttrs.bootstrapMaven ];
-    mavenRepository = buildMavenRepository { dependencies = finalAttrs.passthru.mavenDeps; };
-    doInstallCheck = true;
-    installCheckPhase = checkMavenProvides finalAttrs;
+    #bootstrapMaven = maven_3_3_9_mvn; # TODO
+    jdk = jdk11_headless;
     passthru = prevAttrs.passthru // {
       # also run jq -S '.' on them.
       mavenDeps = mergeDeps [
@@ -55,11 +33,9 @@ maven_nixpkgs.overrideAttrs (
         ./more.json
         (fromGradleLock ./more.lock)
       ];
-    };
-    meta = prevAttrs.meta // {
-      mavenProvides = exposeMavenProvides finalAttrs;
-      # TODO
-      mavenProvidesInternal = { };
+      # also run jq -S '.' on it.
+      # also need to manually remove 3.9.14 entries from linux-m2.json
+      #mavenDeps = ./linux-m2.json;
     };
   }
 )
