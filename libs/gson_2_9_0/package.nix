aca489466edade21b6c6c298fa0806ba95a714c7
Mio <mio@mio19.uk>
2026-04-14 07:54:43 +1000

    Revert "refactor gson_2_2_4"
    This reverts commit acf28fa5054c23576ad15c21865bfe76283f8fdf.

--- libs/gson_2_9_0/package.nix
+++ libs/gson_2_9_0/package.nix
@@ -1,12 +1,5 @@
-{ gson_2_2_4, fetchFromGitHub }:
-gson_2_2_4.overrideAttrs (
-  finalAttrs: prevAttrs: {
-    version = "2.9.0";
-    src = fetchFromGitHub {
-      owner = "google";
-      repo = "gson";
-      tag = "gson-parent-${finalAttrs.version}";
-      hash = "sha256-brO7ae/KwuJkn4M9BUE6BvRv7KC2Zz+fpIWIefDro0k=";
-    };
-  }
-)
+{ gson_common }:
+gson_common {
+  version = "2.9.0";
+  hash = "sha256-brO7ae/KwuJkn4M9BUE6BvRv7KC2Zz+fpIWIefDro0k=";
+}
--- libs/gson_2_9_1/package.nix
+++ libs/gson_2_9_1/package.nix
@@ -1,12 +1,5 @@
-{ gson_2_2_4, fetchFromGitHub }:
-gson_2_2_4.overrideAttrs (
-  finalAttrs: prevAttrs: {
-    version = "2.9.1";
-    src = fetchFromGitHub {
-      owner = "google";
-      repo = "gson";
-      tag = "gson-parent-${finalAttrs.version}";
-      hash = "sha256-glTd/quExpzigpzIqbwbrl5Rgvsf+K/c21REnMcsTXo=";
-    };
-  }
-)
+{ gson_common }:
+gson_common {
+  version = "2.9.1";
+  hash = "sha256-glTd/quExpzigpzIqbwbrl5Rgvsf+K/c21REnMcsTXo=";
+}
