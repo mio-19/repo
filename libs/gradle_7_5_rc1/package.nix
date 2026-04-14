5e3191065b7846d8b97d6266d1b3c9580998d08f
Mio <mio@mio19.uk>
2026-04-15 00:31:59 +1000

    # patch -p1 < repository.patch

--- libs/gradle_7_5_rc1/package.nix
+++ libs/gradle_7_5_rc1/package.nix
@@ -11,4 +11,5 @@
 # git checkout v7.5.0-RC1
 # nix-shell -p javaPackages.compiler.openjdk11-bootstrap
 # rm gradle/verification-metadata.xml
+# patch -p1 < repository.patch
 # nix run github:tadfisher/gradle2nix/6c0f9601ac41a1af04df09d8377ab706d07a4cf4  -- --gradle-wrapper=7.5-rc-1
