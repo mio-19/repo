976ea31a61654bbda5feb5da888556f50e98f2ea
Mio <mio@mio19.uk>
2026-04-14 13:33:36 +1000

    save

---
+++ libs/gradle-unwrapped_5_6_4/package.nix
@@ -0,0 +1,39 @@
+{
+  gradle_5_1_1,
+  jdk11_headless,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+}:
+let
+  gradle = gradle_5_1_1;
+in
+# https://github.com/NixOS/nixpkgs/blob/4ed2dff2b5c2970997ed3a12aae50181a352f719/doc/languages-frameworks/gradle.section.md
+stdenv.mkDerivation (finalAttrs: {
+  pname = "gradle-unwrapped";
+  version = "5.6.4";
+  src = fetchFromGitHub {
+    owner = "gradle";
+    repo = "gradle";
+    rev = "v${finalAttrs.version}";
+    hash = "";
+  };
+
+  nativeBuildInputs = [
+    gradle
+    makeWrapper
+  ];
+# $(nix build .#gradle_5_6_4_unwrapped.mitmCache.updateScript --no-link --print-out-paths)
+  mitmCache = gradle.fetchDeps {
+    inherit (finalAttrs) pname;
+    pkg = finalAttrs.finalPackage;
+    data = ./deps.json;
+  };
+  # this is required for using mitm-cache on Darwin
+  __darwinAllowLocalNetworking = true;
+  gradleFlags = [ "-Dfile.encoding=utf-8" ];
+  installPhase = ''
+    mkdir -p $out
+    cp -r  build/libs/ $out/
+  '';
+})
--- libs/kotlin-stdlib_2_3_20/package.nix
+++ libs/kotlin-stdlib_2_3_20/package.nix
@@ -1,20 +1,23 @@
 {
-  gradle_5_1_1,
+  gradle_8_14,
   jdk11_headless,
   stdenv,
   fetchFromGitHub,
   makeWrapper,
 }:
-let
-  gradle = gradle_5_1_1;
-in
 # https://github.com/NixOS/nixpkgs/blob/4ed2dff2b5c2970997ed3a12aae50181a352f719/doc/languages-frameworks/gradle.section.md
-stdenv.mkDerivation (finalAttrs: {
-  pname = "gradle-unwrapped";
-  version = "5.6.4";
+stdenv.mkDerivation (finalAttrs: 
+let
+  inherit (finalAttrs) gradle;
+in{
+  # https://github.com/JetBrains/kotlin/blob/v2.3.20/gradle/wrapper/gradle-wrapper.properties
+  gradle = gradle_8_14;
+  pname = "kotlin-stdlib";
+  version = "2.3.20";
+  
   src = fetchFromGitHub {
-    owner = "gradle";
-    repo = "gradle";
+    owner = "JetBrains";
+    repo = "kotlin";
     rev = "v${finalAttrs.version}";
     hash = "";
   };
@@ -23,9 +26,10 @@ stdenv.mkDerivation (finalAttrs: {
     gradle
     makeWrapper
   ];
-# $(nix build .#gradle_5_6_4_unwrapped.mitmCache.updateScript --no-link --print-out-paths)
+# $(nix build .#kotlin-stdlib_2_3_20.mitmCache.updateScript --no-link --print-out-paths)
   mitmCache = gradle.fetchDeps {
     inherit (finalAttrs) pname;
+    pkg = finalAttrs.finalPackage;
     data = ./deps.json;
   };
   # this is required for using mitm-cache on Darwin
