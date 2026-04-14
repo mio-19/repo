b71f6d86c4e81525ba144f24d2835ee9bf27b34d
Mio <mio@mio19.uk>
2026-04-14 12:42:15 +1000

    8_14_4 jdk17_headless

--- libs/gradle_8_14_4/package.nix
+++ libs/gradle_8_14_4/package.nix
@@ -1,5 +1,5 @@
 {
-  jdk11_headless,
+  jdk17_headless,
   jdk21_headless,
   gradle_8_14,
   gradle-from-source,
@@ -9,7 +9,7 @@ gradle-from-source {
   hash = "sha256-DzIJ4bPmyOj58GEpdAS9MZ4APmT5NKgNhiNp/UwZbhY=";
   lockFile = ./gradle.lock;
   defaultJava = jdk21_headless;
-  buildJdk = jdk11_headless;
+  buildJdk = jdk17_headless;
   # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.14
   bootstrapGradle = gradle_8_14;
 }
