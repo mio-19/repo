79235622c1d9c4ff62d2bd61ed98a5c0471718d2
Mio <mio@mio19.uk>
2026-04-16 11:19:22 +1000

    recursiveUpdateUntil

--- app/by-name/deepMerge/package.nix
+++ app/by-name/deepMerge/package.nix
@@ -1,23 +1,15 @@
 { lib }:
 let
+  inherit (lib) recursiveUpdateUntil;
   isAttrsNotDer = x: builtins.isAttrs x && !lib.isDerivation x;
   # https://discourse.nixos.org/t/nix-function-to-merge-attributes-records-recursively-and-concatenate-arrays/2030/9
+  # https://noogle.dev/f/lib/recursiveUpdate
   deepMerge =
     lhs: rhs:
-    lhs
-    // rhs
-    // (builtins.mapAttrs (
-      rName: rValue:
-      let
-        lValue = lhs.${rName} or null;
-      in
-      if isAttrsNotDer lValue && isAttrsNotDer rValue then
-        deepMerge lValue rValue
-      else if builtins.isList lValue && builtins.isList rValue then
-        lValue ++ rValue
-      else
-        rValue
-    ) rhs);
+    recursiveUpdateUntil (
+      path: lhs: rhs:
+      !(isAttrsNotDer lhs && isAttrsNotDer rhs)
+    ) lhs rhs;
   deepMerges =
     xs:
     if !lib.isList xs then
