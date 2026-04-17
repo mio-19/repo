55010ef7027dc768e446f8253fda3cf180d430c0
Mio <mio@mio19.uk>
2026-04-17 13:05:49 +1000

    save

--- libs/gradle_7_6_20220719/package.nix
+++ libs/gradle_7_6_20220719/package.nix
@@ -4,8 +4,7 @@
   temurin-bin-11,
   temurin-bin-17,
   jdk17_headless,
-  gradle_7_6_20220624,
-  gradle_7_6,
+  gradle_7_6_20220622,
   gradle-from-source,
   mergeLock,
 }:
@@ -14,15 +13,15 @@ gradle-from-source {
   rev = "da4e5b63e398bf72dee9cd328077fb71d7d0d142";
   hash = "sha256-reCiizzoZvg1jHRWRapDUcSvRAyaiuf4iaZSYUecjcg=";
   lockFile = mergeLock [
-    gradle_7_6_20220624.unwrapped.passthru.lockFile
+    gradle_7_6_20220622.unwrapped.passthru.lockFile
     ./more.gradle.lock
   ];
   defaultJava = jdk17_headless;
   # this version specifically ask for termurin branded jdk.
   buildJdk = temurin-bin-11;
   patches = [
-    ./fix-test-fixtures-artifact.patch
-    ./fix-arch-test-dependency.patch
+    #./fix-test-fixtures-artifact.patch
+    #./fix-arch-test-dependency.patch
   ];
   javaToolchains = [
     temurin-bin-8
@@ -35,5 +34,5 @@ gradle-from-source {
     rm gradle/verification-*
     nix run github:tadfisher/gradle2nix/6c0f9601ac41a1af04df09d8377ab706d07a4cf4  -- --gradle-wrapper=7.5-rc-1
   */
-  bootstrapGradle = gradle_7_6;
+  bootstrapGradle = gradle_7_6_20220622;
 }
