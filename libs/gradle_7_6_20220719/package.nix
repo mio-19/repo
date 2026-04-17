235e8ada1c7232ce6080584bbf3d22213dcc0cbd
Mio <mio@mio19.uk>
2026-04-17 12:18:42 +1000

    savewhatever

--- libs/gradle_7_6_20220719/package.nix
+++ libs/gradle_7_6_20220719/package.nix
@@ -4,17 +4,26 @@
   temurin-bin-11,
   temurin-bin-17,
   jdk17_headless,
+  gradle_7_6_20220624,
   gradle_7_6,
   gradle-from-source,
+  mergeLock,
 }:
 gradle-from-source {
   version = "7.6.0-20220719";
   rev = "da4e5b63e398bf72dee9cd328077fb71d7d0d142";
-  hash = "";
-  lockFile = { };
+  hash = "sha256-reCiizzoZvg1jHRWRapDUcSvRAyaiuf4iaZSYUecjcg=";
+  lockFile = mergeLock [
+    gradle_7_6_20220624.unwrapped.passthru.lockFile
+    ./more.gradle.lock
+  ];
   defaultJava = jdk17_headless;
   # this version specifically ask for termurin branded jdk.
   buildJdk = temurin-bin-11;
+  patches = [
+    ./fix-test-fixtures-artifact.patch
+    ./fix-arch-test-dependency.patch
+  ];
   javaToolchains = [
     temurin-bin-8
     temurin-bin-11
