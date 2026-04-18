7b9c74b26981cf0c426a200b0252ea070ce0dc8b
Mio <mio@mio19.uk>
2026-04-18 17:02:57 +1000

    fixup robotnix?

--- robotnix.nix
+++ robotnix.nix
@@ -1,63 +1,10 @@
 {
   self,
-  inputsPatched,
-  pkgsPatched,
+  inputs,
   ...
 }:
 let
-  inherit (inputsPatched) nixpkgs robotnix nix-github-actions;
-  mkGos =
-    { ccache }:
-    let
-      common =
-        f:
-        args@{ pkgs, ... }:
-        {
-          _module.args.pkgs-unfree = pkgsPatched;
-          _module.args.robotnix = robotnix;
-          _module.args.self = self;
-          imports = [ f ];
-          ccache.enable = ccache;
-        };
-    in
-    # https://github.com/MatthewCroughan/nixcfg/blob/afab322e6da20cc038d8577dd4a365673702d183/flake.nix#L57
-    nixpkgs.lib.mapAttrs (n: v: robotnix.lib.robotnixSystem v) {
-      akita = common ./gos_akita.nix;
-      husky = common ./gos_husky.nix;
-      mustang = common ./gos_mustang.nix;
-      tangorpro = common ./gos_tangorpro.nix;
-      cheetah = common ./gos_cheetah.nix;
-      caiman = common ./gos_caiman.nix;
-    };
-  mkLos =
-    { ccache }:
-    let
-      common =
-        f:
-        args@{ pkgs, ... }:
-        {
-          _module.args.pkgs-unfree = pkgsPatched;
-          _module.args.robotnix = robotnix;
-          _module.args.self = self;
-          imports = [ f ];
-          ccache.enable = ccache;
-        };
-    in
-    # https://github.com/MatthewCroughan/nixcfg/blob/afab322e6da20cc038d8577dd4a365673702d183/flake.nix#L57
-    nixpkgs.lib.mapAttrs (n: v: robotnix.lib.robotnixSystem v) {
-      gta4xlwifi = common ./gta4xlwifi.nix;
-      enchilada = common ./enchilada.nix;
-      enchilada_derpfest16 = common ./enchilada-derpfest16.nix;
-      enchilada_mainline = common ./enchilada-mainline.nix;
-      nx_tab = common ./nx_tab.nix;
-      utm = common ./utm.nix;
-      akita = common ./akita.nix;
-      dm3q-kenzie = common ./dm3q-kenzie.nix;
-      dm3q_cola2261 = common ./dm3q-cola2261.nix;
-      gts9wifi = common ./gts9wifi.nix;
-      gts7lwifi = common ./gts7lwifi.nix;
-      gts7l = common ./gts7l.nix;
-    };
+  inherit (inputs) nixpkgs nix-github-actions;
 in
 {
   imports = [ ./kernels.nix ];
@@ -68,32 +15,86 @@ in
         (mapAttrs' (name: cfg: nameValuePair "${name}-ota" cfg.ota) losNoCcache)
         // (mapAttrs' (name: cfg: nameValuePair "${name}-img" cfg.img) losNoCcache);
     };
-    los = mkLos {
-      ccache = true;
-    };
-    gos = mkGos {
-      ccache = true;
-    };
-    gosNoCcache = mkGos {
-      ccache = false;
-    };
-    losNoCcache = mkLos {
-      ccache = false;
-    };
+    inherit (self.legacyPackages.x86_64-linux)
+      los
+      gos
+      losNoCcache
+      gosNoCcache
+      ;
   };
   perSystem =
-    args@{ pkgs, ... }:
+    { inputsPatched, pkgsPatched, ... }:
     let
-      sources = (import ./_sources/generated.nix) {
-        inherit (pkgs)
-          fetchurl
-          fetchgit
-          fetchFromGitHub
-          dockerTools
-          ;
-      };
+      inherit (inputsPatched) nixpkgs robotnix;
+      mkGos =
+        { ccache }:
+        let
+          common =
+            f:
+            { ... }:
+            {
+              _module.args.pkgs-unfree = pkgsPatched;
+              _module.args.robotnix = robotnix;
+              _module.args.self = self;
+              imports = [ f ];
+              ccache.enable = ccache;
+            };
+        in
+        # https://github.com/MatthewCroughan/nixcfg/blob/afab322e6da20cc038d8577dd4a365673702d183/flake.nix#L57
+        nixpkgs.lib.mapAttrs (n: v: robotnix.lib.robotnixSystem v) {
+          akita = common ./gos_akita.nix;
+          husky = common ./gos_husky.nix;
+          mustang = common ./gos_mustang.nix;
+          tangorpro = common ./gos_tangorpro.nix;
+          cheetah = common ./gos_cheetah.nix;
+          caiman = common ./gos_caiman.nix;
+        };
+      mkLos =
+        { ccache }:
+        let
+          common =
+            f:
+            { ... }:
+            {
+              _module.args.pkgs-unfree = pkgsPatched;
+              _module.args.robotnix = robotnix;
+              _module.args.self = self;
+              imports = [ f ];
+              ccache.enable = ccache;
+            };
+        in
+        # https://github.com/MatthewCroughan/nixcfg/blob/afab322e6da20cc038d8577dd4a365673702d183/flake.nix#L57
+        nixpkgs.lib.mapAttrs (n: v: robotnix.lib.robotnixSystem v) {
+          gta4xlwifi = common ./gta4xlwifi.nix;
+          enchilada = common ./enchilada.nix;
+          enchilada_derpfest16 = common ./enchilada-derpfest16.nix;
+          enchilada_mainline = common ./enchilada-mainline.nix;
+          nx_tab = common ./nx_tab.nix;
+          utm = common ./utm.nix;
+          akita = common ./akita.nix;
+          dm3q-kenzie = common ./dm3q-kenzie.nix;
+          dm3q_cola2261 = common ./dm3q-cola2261.nix;
+          gts9wifi = common ./gts9wifi.nix;
+          gts7lwifi = common ./gts7lwifi.nix;
+          gts7l = common ./gts7l.nix;
+        };
+      pkgs = pkgsPatched;
     in
     {
+      legacyPackages = {
+        los = mkLos {
+          ccache = true;
+        };
+        gos = mkGos {
+          ccache = true;
+        };
+        gosNoCcache = mkGos {
+          ccache = false;
+        };
+        losNoCcache = mkLos {
+          ccache = false;
+        };
+      };
       packages.grapheneos-husky-kernel = pkgs.callPackage ./gos_kernel_shusky.nix { };
       packages.grapheneos-mustang-kernel = pkgs.callPackage ./gos_mustang_kernel.nix { };
       packages.grapheneos-tangorpro-kernel = pkgs.callPackage ./gos_tangorpro_kernel.nix { };
