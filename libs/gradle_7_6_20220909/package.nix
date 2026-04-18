976c67bda8efb28092a32d642de47365235bde56
Mio <mio@mio19.uk>
2026-04-18 11:50:59 +1000

    fix gradle 20220909 snapshot build

--- libs/gradle_7_6_20220909/package.nix
+++ libs/gradle_7_6_20220909/package.nix
@@ -4,14 +4,18 @@
   temurin-bin-11,
   temurin-bin-17,
   jdk17_headless,
-  gradle_7_6,
   gradle-from-source,
+  gradle_7_6_20220831,
+  mergeLock,
 }:
 gradle-from-source {
   version = "7.6.0-20220909";
   rev = "f8b0dfe64a1adb979d594102dac01435901444a4";
-  hash = "";
-  lockFile = { };
+  hash = "sha256-5340t8B1eNPJNair9FC7fo5IL6FHPaiyMk3Yt8W7HM0=";
+  lockFile = mergeLock [
+    gradle_7_6_20220831.unwrapped.passthru.lockFile
+    ./more.gradle.lock
+  ];
   defaultJava = jdk17_headless;
   # this version specifically ask for termurin branded jdk.
   buildJdk = temurin-bin-11;
@@ -26,5 +30,5 @@ gradle-from-source {
     rm gradle/verification-*
     nix run github:tadfisher/gradle2nix/6c0f9601ac41a1af04df09d8377ab706d07a4cf4  -- --gradle-wrapper=7.5-rc-1
   */
-  bootstrapGradle = gradle_7_6;
+  bootstrapGradle = gradle_7_6_20220831;
 }
