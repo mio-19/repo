e1ea2ae7009a3f36be80a61ae7515effe294a049
Mio <mio@mio19.uk>
2026-04-18 16:50:42 +1000

    build gradle 7.6 from source

--- libs/gradle_7_6_rc1/package.nix
+++ libs/gradle_7_6_rc1/package.nix
@@ -4,14 +4,15 @@
   temurin-bin-17,
   jdk17_headless,
   gradle-from-source,
-  gradle_7_6_rc4,
+  gradle_7_6_M1,
   mergeLock,
 }:
 gradle-from-source {
-  version = "7.6.0";
-  hash = "sha256-RWEALk7H51a3ztnA6UFJVLjQIthfpruP1e22TD/LnR8=";
+  version = "7.6.0-RC1";
+  hash = "sha256-CWHRq84wSXUiPkKOHnRLyFdAH7GME1kw4cA5WbQgolE=";
   lockFile = mergeLock [
-    gradle_7_6_rc4.unwrapped.passthru.lockFile
+    gradle_7_6_M1.unwrapped.passthru.lockFile
+    ./more.gradle.lock
   ];
   defaultJava = jdk17_headless;
   # this version specifically ask for termurin branded jdk.
@@ -21,5 +22,5 @@ gradle-from-source {
     temurin-bin-11
     temurin-bin-17
   ];
-  bootstrapGradle = gradle_7_6_rc4;
+  bootstrapGradle = gradle_7_6_M1;
 }
