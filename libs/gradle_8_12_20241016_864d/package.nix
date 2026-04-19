65349c7330d6774cec8ae63130cd924419336309
Mio <mio@mio19.uk>
2026-04-19 19:36:10 +1000

    gradle_8_12_1: go jdk

--- libs/gradle_8_12_20241016_864d/package.nix
+++ libs/gradle_8_12_20241016_864d/package.nix
@@ -1,14 +1,10 @@
 # this is before gradle_12_rc1. https://github.com/gradle/gradle/commit/864ddaf0a289b122e804046ab4a0e618dce9b8e7
 {
-  temurin-bin-8,
-  temurin-bin-11,
-  temurin-bin-17,
+  jdk17_headless,
   jdk21_headless,
   gradle_8_12_20241015,
   gradle-from-source,
   mergeLock,
-  jq,
-  lib,
 }:
 gradle-from-source {
   version = "8.12-20241016-864d";
@@ -19,12 +15,26 @@ gradle-from-source {
     ../gradle_8_12_1/gradle.lock
   ];
   defaultJava = jdk21_headless;
-  # this version specifically ask for termurin branded jdk.
-  buildJdk = temurin-bin-17;
-  javaToolchains = [
-    temurin-bin-8
-    temurin-bin-11
-    temurin-bin-17
-  ];
+  buildJdk = jdk17_headless;
   bootstrapGradle = gradle_8_12_20241015;
+  postPatch =
+    let
+      # grep -rl 'vendor = JvmVendorSpec.ADOPTIUM' . | sed 's/.*/"&"/'
+      files = [
+        "./platforms/documentation/docs/src/snippets/java/toolchain-filters/kotlin/build.gradle.kts"
+        "./platforms/documentation/docs/src/snippets/java/toolchain-filters/groovy/build.gradle"
+        "./platforms/jvm/language-java/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy"
+        "./build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild/commons/JavaPluginExtensions.kt"
+        "./build-logic-commons/settings.gradle.kts"
+        "./build-logic-settings/build-environment/build.gradle.kts"
+        "./build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts"
+      ];
+    in
+    ''
+      substituteInPlace ${builtins.concatStringsSep " " files} \
+        --replace-fail 'vendor = JvmVendorSpec.ADOPTIUM' ""
+
+      substituteInPlace gradle/gradle-daemon-jvm.properties \
+        --replace-fail 'toolchainVendor=adoptium' ""
+    '';
 }
