d6fe57e8164809ade673f56b8f7d22be8d26182d
Mio <mio@mio19.uk>
2026-04-22 19:18:42 +1000

    libs/gradle: restore 7.x source bootstrap chain

--- libs/gradle_7_5_rc1/package.nix
+++ libs/gradle_7_5_rc1/package.nix
@@ -1,15 +1,55 @@
 {
+  temurin-bin-8,
+  temurin-bin-11,
+  temurin-bin-17,
   jdk17_headless,
-  gradle-packages,
+  gradle-from-source,
+  gradle_7_4_2,
+  mergeLock,
 }:
-(gradle-packages.mkGradle {
-  version = "7.5-rc-1";
-  hash = "sha256-i6V6N+HguMQV5NkXGNUQNSI6pzExz3GaUMlaKogmnrI=";
+gradle-from-source {
+  version = "7.5.0-RC1";
+  hash = "sha256-22eR97X9z6QhN4yMfaAzElErs1A/vCGqcRyfL4L2MwE=";
+  lockFile = mergeLock [
+    gradle_7_4_2.unwrapped.passthru.lockFile
+    ./gradle.lock
+  ];
   defaultJava = jdk17_headless;
-}).wrapped
+  buildJdk = temurin-bin-11;
+  javaToolchains = [
+    temurin-bin-8
+    temurin-bin-11
+    temurin-bin-17
+  ];
+  bootstrapGradle = gradle_7_4_2;
+  patches = [
+    ./repository.patch
+    ./bootstrap-compat.patch
+  ];
+  postPatch = ''
+        rm -f gradle/verification-keyring.keys gradle/verification-metadata.xml
 
-# git checkout v7.5.0-RC1
-# nix-shell -p javaPackages.compiler.openjdk11-bootstrap
-# rm gradle/verification-metadata.xml
-# patch -p1 < repository.patch
-# nix run github:tadfisher/gradle2nix/6c0f9601ac41a1af04df09d8377ab706d07a4cf4  -- --gradle-wrapper=7.5-rc-1
+        echo "kotlin.js.yarn.download=false" >> gradle.properties
+        echo "kotlin.js.node.download=false" >> gradle.properties
+
+        cat <<EOF >> settings.gradle.kts
+    gradle.rootProject {
+        allprojects {
+            tasks.configureEach {
+                if (name == "browserProductionWebpack") {
+                    actions.clear()
+                    doLast {
+                        val jsFile = file("build/distributions/configuration-cache-report.js")
+                        jsFile.parentFile.mkdirs()
+                        jsFile.writeText("")
+                    }
+                } else if (name == "rootPackageJson" || name == "kotlinNodeJsSetup" || name == "kotlinNpmInstall" || name == "generateExternalsIntegrated" || name == "packageJson") {
+                    enabled = false
+                    onlyIf { false }
+                }
+            }
+        }
+    }
+    EOF
+  '';
+}
--- libs/gradle_7_6_20220514/package.nix
+++ libs/gradle_7_6_20220514/package.nix
@@ -6,12 +6,16 @@
   jdk17_headless,
   gradle_7_5_rc1,
   gradle-from-source,
+  mergeLock,
 }:
 gradle-from-source {
   version = "7.6.0-20220514";
   rev = "d2892b427be7c9438ea260c3aac331e318062c6b";
   hash = "sha256-pSv7aKwRDw8Mys48A3cIR+4e28m+SIX8aVEZgARlxZA=";
-  lockFile = ./gradle.lock;
+  lockFile = mergeLock [
+    gradle_7_5_rc1.unwrapped.passthru.lockFile
+    ./gradle.lock
+  ];
   defaultJava = jdk17_headless;
   # this version specifically ask for termurin branded jdk.
   buildJdk = temurin-bin-11;
