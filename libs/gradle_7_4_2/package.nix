bfa442a7bf0054c46373bec20ca9e44b9f73e0ff
Mio <mio@mio19.uk>
2026-04-22 14:02:27 +1000

    libs/gradle: add source-built gradle_7_4_2 bridge

---
+++ libs/gradle_7_4_2/package.nix
@@ -0,0 +1,48 @@
+{
+  temurin-bin-8,
+  temurin-bin-11,
+  temurin-bin-17,
+  jdk17_headless,
+  gradle-from-source,
+  gradle_7_2,
+}:
+gradle-from-source {
+  version = "7.4.2";
+  hash = "sha256-nsbNw0tgpbPKtrLvOOuipXQxPa3YGrwuz77uxsPiMug=";
+  lockFile = ./gradle.lock;
+  defaultJava = jdk17_headless;
+  buildJdk = temurin-bin-11;
+  javaToolchains = [
+    temurin-bin-8
+    temurin-bin-11
+    temurin-bin-17
+  ];
+  bootstrapGradle = gradle_7_2;
+  patches = [
+    ./bootstrap-compat.patch
+  ];
+  postPatch = ''
+    echo "kotlin.js.yarn.download=false" >> gradle.properties
+    echo "kotlin.js.node.download=false" >> gradle.properties
+
+    cat <<EOF >> settings.gradle.kts
+gradle.rootProject {
+    allprojects {
+        tasks.configureEach {
+            if (name == "browserProductionWebpack") {
+                actions.clear()
+                doLast {
+                    val jsFile = file("build/distributions/configuration-cache-report.js")
+                    jsFile.parentFile.mkdirs()
+                    jsFile.writeText("")
+                }
+            } else if (name == "rootPackageJson" || name == "kotlinNodeJsSetup" || name == "kotlinNpmInstall" || name == "generateExternalsIntegrated" || name == "packageJson") {
+                enabled = false
+                onlyIf { false }
+            }
+        }
+    }
+}
+EOF
+  '';
+}
