81d2b9c4249bf2a6737e93cce4f0fbccb1ec5ed3
Mio <mio@mio19.uk>
2026-05-07 19:55:33 +1000

    s

--- app/apks/youtube-music-morphe/package.nix
+++ app/apks/youtube-music-morphe/package.nix
@@ -13,7 +13,7 @@ let
         # https://youtube-music.en.uptodown.com/android/download/1164645913-x
         name = "youtube-music-8.47.56.apk";
         url = "https://web.archive.org/web/20260507094903if_/https://dw.uptodown.net/dwn/MFhQ8_gxRt-iwo-ip3SSEvXz2URDWbEvJa1VW5E5CY00eaurN8S5euBXJStp8VNfRc6T_8UPIBZzZDDRsjTsw-UsqM4QHo__2GOB25F3d6HKuP_WvTUFqp8vIi110pnN/uMMoLcNJq_uDd3yis7FL-VEta8MdZQXkGUsKlsa7XnAW61zplE8l3J6Myzv8993gAHuHbqcWfSWqbx9d9GNLSjd4TwTmAUV9Bw6tk3mo1Ju6Ze_SBa6aPGjN5gQJBlEn/pxOEgaYm2OPaBFn76E2LzVBd96O6d_0rtEKf0gjLcLbXF9bt0U4fkEUw08SOmschh_mCqlDtxrXCmsilH6qEwenDPf56d17EhJzaeYG2dZY=/youtube-music-8-47-56.apk";
-        hash = "";
+        hash = "sha256-yTTvRQ3TywSpOh6EKmdRJTihAckBFUION8RndH6P7NI=";
       };
 
       morphePatches = "${morphe-patches}/patches-${morphe-patches.version}.mpp";
