ba0f799745e3222c14be2e5a2f75818fc23113db
Mio <mio@mio19.uk>
2026-05-07 19:54:47 +1000

    s

--- app/apks/youtube-morphe/package.nix
+++ app/apks/youtube-morphe/package.nix
@@ -10,10 +10,10 @@ let
   appPackage =
     let
       youtubeApk = fetchurl {
-        # APKPure page: https://apkpure.com/youtube-2025/com.google.android.youtube/download/21.16.256
-        name = "youtube-21.16.256.apk";
-        url = "https://web.archive.org/web/20260507092807if_/https://d-e03.winudf.com/b/APK/Y29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmVfMTU2MTA2ODQxMl9mZGIzNmU1NQ?_fn=WW91VHViZV8yMS4xNi4yNTZfQVBLUHVyZS5hcGs&_p=Y29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmU%3D&download_id=1890301786123761&is_hot=true&k=b08aa5fb268cbf1a3673f6053e55447769fdac65&uu=https%3A%2F%2Fd-07.winudf.com%2Fb%2FAPK%2FY29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmVfMTU2MTA2ODQxMl9mZGIzNmU1NQ%3Fk%3Dafdf11c16fa6384c7ceb14c88b7e2d4c69fdac65";
-        hash = "sha256-ck0r8V0x2smNsA2CkU2zh27fZuYv4oRQAfIEIJ7PTAA=";
+        # https://youtube.en.uptodown.com/android/download/1165342360
+        name = "youtube-20.47.62.apk";
+        url = "https://web.archive.org/web/20260507095312if_/https://dw.uptodown.net/dwn/IdhnqP6xT2dcasDhCpJg-1yfGw-8LuHpOvCHJQPRcEQRfjAshlfmJk5eMZEMFkLUFiDymwMcjGX7LS7GFgL8CFrZJ8ejG9wR6IeOm26dQIUKipReOhMLgwtXT_EPK3wJ/nI3U-UOyc3J3D4GJmZrN6DPh42TLqFmNYYdo4bpI_Jyq5F6lzwfaY4aqHGGO8tPIt3hX1qIW8eX7nsPCnJDZAJbKyrLmPaSdrKxGHc4KLCsZ2d9Wb6KcSM0YK94vnXis/E1bjO47fwVYrrSIV5f4pB0v9tSuWUqMpJWHBjjQNdawIIHSdbOuhoTnLW-CHFaCkUD9_ZmWOQC1WPMm-ARgmOw==/youtube-20-47-62.apk";
+        hash = "";
       };
 
       morphePatches = "${morphe-patches}/patches-${morphe-patches.version}.mpp";
