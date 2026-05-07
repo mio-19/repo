ebb5d075ef72fac6a7f70244274f8d3ac73a1d1c
Mio <mio@mio19.uk>
2026-05-07 19:35:33 +1000

    s

--- app/apks/youtube-morphe/package.nix
+++ app/apks/youtube-morphe/package.nix
@@ -13,7 +13,7 @@ let
         # APKPure page: https://apkpure.com/youtube-2025/com.google.android.youtube/download/21.16.256
         name = "youtube-21.16.256.apk";
         url = "https://web.archive.org/web/20260507092807if_/https://d-e03.winudf.com/b/APK/Y29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmVfMTU2MTA2ODQxMl9mZGIzNmU1NQ?_fn=WW91VHViZV8yMS4xNi4yNTZfQVBLUHVyZS5hcGs&_p=Y29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmU%3D&download_id=1890301786123761&is_hot=true&k=b08aa5fb268cbf1a3673f6053e55447769fdac65&uu=https%3A%2F%2Fd-07.winudf.com%2Fb%2FAPK%2FY29tLmdvb2dsZS5hbmRyb2lkLnlvdXR1YmVfMTU2MTA2ODQxMl9mZGIzNmU1NQ%3Fk%3Dafdf11c16fa6384c7ceb14c88b7e2d4c69fdac65";
-        hash = "";
+        hash = "sha256-ck0r8V0x2smNsA2CkU2zh27fZuYv4oRQAfIEIJ7PTAA=";
       };
 
       morphePatches = "${morphe-patches}/patches-${morphe-patches.version}.mpp";
