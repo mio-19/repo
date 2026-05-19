e7446f0202a9ab2a7830abd2c14e13d46e7cf450
Mio <mio@mio19.uk>
2026-05-19 16:46:19 +1000

    gradle_8_2: use source JDKs

--- libs/gradle_8_2/package.nix
+++ libs/gradle_8_2/package.nix
@@ -1,7 +1,7 @@
 {
-  temurin-bin-8,
-  temurin-bin-11,
-  temurin-bin-17,
+  jdk8_headless,
+  jdk11_headless,
+  jdk17_headless,
   jdk21_headless,
   gradle_8_2_bootstrap,
   gradle-from-source,
@@ -15,15 +15,29 @@ gradle-from-source {
     ./more.gradle.lock
   ];
   defaultJava = jdk21_headless;
-  # this version specifically ask for termurin branded jdk.
-  buildJdk = temurin-bin-11;
+  buildJdk = jdk11_headless;
   javaToolchains = [
-    temurin-bin-8
-    temurin-bin-11
-    temurin-bin-17
+    jdk8_headless
+    jdk11_headless
+    jdk17_headless
   ];
   bootstrapGradle = gradle_8_2_bootstrap;
   gradleFlags = [
     "-PbuildKotlinVersion=1.8.20"
   ];
+  postPatch = ''
+    for file in \
+      build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts \
+      build-logic-commons/code-quality-rules/build.gradle.kts \
+      build-logic-commons/commons/build.gradle.kts \
+      build-logic-commons/commons/src/main/kotlin/common.kt \
+      build-logic-commons/gradle-plugin/build.gradle.kts \
+      subprojects/docs/src/snippets/java/toolchain-filters/groovy/build.gradle \
+      subprojects/platform-jvm/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy
+    do
+      if [ -f "$file" ] && grep -Fq 'vendor = JvmVendorSpec.ADOPTIUM' "$file"; then
+        substituteInPlace "$file" --replace-fail 'vendor = JvmVendorSpec.ADOPTIUM' ""
+      fi
+    done
+  '';
 }
