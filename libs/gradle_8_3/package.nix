96056835a96e7617b750065864fe0184fa098a8f
Mio <mio@mio19.uk>
2026-05-19 16:35:01 +1000

    gradle_8_3: use source JDKs

--- libs/gradle_8_3/package.nix
+++ libs/gradle_8_3/package.nix
@@ -1,7 +1,7 @@
 {
-  temurin-bin-8,
-  temurin-bin-11,
-  temurin-bin-17,
+  jdk8_headless,
+  jdk11_headless,
+  jdk17_headless,
   jdk21_headless,
   gradle_8_3_20230706,
   gradle-from-source,
@@ -15,12 +15,34 @@ gradle-from-source {
     ./gradle.lock
   ];
   defaultJava = jdk21_headless;
-  # this version specifically ask for termurin branded jdk.
-  buildJdk = temurin-bin-11;
+  buildJdk = jdk11_headless;
   javaToolchains = [
-    temurin-bin-8
-    temurin-bin-11
-    temurin-bin-17
+    jdk8_headless
+    jdk11_headless
+    jdk17_headless
   ];
   bootstrapGradle = gradle_8_3_20230706;
+  postPatch = ''
+    for file in \
+      build-logic-settings/build-environment/build.gradle.kts \
+      build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts \
+      build-logic-commons/basics/build.gradle.kts \
+      build-logic-commons/code-quality-rules/build.gradle.kts \
+      build-logic-commons/commons/build.gradle.kts \
+      build-logic-commons/commons/src/main/kotlin/common.kt \
+      build-logic-commons/gradle-plugin/build.gradle.kts \
+      build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild/commons/JavaPluginExtensions.kt \
+      build-logic-commons/module-identity/build.gradle.kts \
+      platforms/documentation/docs/src/snippets/java/toolchain-filters/groovy/build.gradle \
+      platforms/documentation/docs/src/snippets/java/toolchain-filters/kotlin/build.gradle.kts \
+      platforms/jvm/language-java/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy \
+      subprojects/docs/src/snippets/java/toolchain-filters/groovy/build.gradle \
+      subprojects/docs/src/snippets/java/toolchain-filters/kotlin/build.gradle.kts \
+      subprojects/platform-jvm/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy
+    do
+      if [ -f "$file" ] && grep -Fq 'vendor = JvmVendorSpec.ADOPTIUM' "$file"; then
+        substituteInPlace "$file" --replace-fail 'vendor = JvmVendorSpec.ADOPTIUM' ""
+      fi
+    done
+  '';
 }
