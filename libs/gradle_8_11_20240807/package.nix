31e26077e89ebf2cf7b0837de968d37e64bec983
Mio <mio@mio19.uk>
2026-05-19 10:57:06 +1000

    gradle_8_11_20240807: use source JDKs

--- libs/gradle_8_11_20240807/package.nix
+++ libs/gradle_8_11_20240807/package.nix
@@ -1,8 +1,8 @@
 # this is before gradle v8.11.0-M1. before commit https://github.com/gradle/gradle/commit/a2b280e1291afb5dc49d2b67837390dd5b85c6ed
 {
-  temurin-bin-8,
-  temurin-bin-11,
-  temurin-bin-17,
+  jdk8_headless,
+  jdk11_headless,
+  jdk17_headless,
   jdk21_headless,
   gradle_8_10,
   gradle-from-source,
@@ -13,13 +13,30 @@ gradle-from-source {
   hash = "sha256-08AJLr/ffus4gczX0DQnSIUfGJEGxwpII8PJOlwM9J8=";
   lockFile = ./gradle.lock;
   defaultJava = jdk21_headless;
-  # this version specifically ask for termurin branded jdk.
-  buildJdk = temurin-bin-11;
+  buildJdk = jdk11_headless;
   javaToolchains = [
-    temurin-bin-8
-    temurin-bin-11
-    temurin-bin-17
+    jdk8_headless
+    jdk11_headless
+    jdk17_headless
   ];
+  postPatch = ''
+    for file in \
+      build-logic-settings/build-environment/build.gradle.kts \
+      build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts \
+      build-logic-commons/basics/build.gradle.kts \
+      build-logic-commons/code-quality-rules/build.gradle.kts \
+      build-logic-commons/gradle-plugin/build.gradle.kts \
+      build-logic-commons/gradle-plugin/src/main/kotlin/gradlebuild/commons/JavaPluginExtensions.kt \
+      build-logic-commons/module-identity/build.gradle.kts \
+      platforms/documentation/docs/src/snippets/java/toolchain-filters/groovy/build.gradle \
+      platforms/documentation/docs/src/snippets/java/toolchain-filters/kotlin/build.gradle.kts \
+      platforms/jvm/language-java/src/integTest/groovy/org/gradle/jvm/toolchain/JavaToolchainDownloadIntegrationTest.groovy
+    do
+      if [ -f "$file" ] && grep -Fq 'vendor = JvmVendorSpec.ADOPTIUM' "$file"; then
+        substituteInPlace "$file" --replace-fail 'vendor = JvmVendorSpec.ADOPTIUM' ""
+      fi
+    done
+  '';
   # nix-shell -p javaPackages.compiler.openjdk11-bootstrap
   # nix run github:tadfisher/gradle2nix/v2  -- --gradle-wrapper=8.10
   bootstrapGradle = gradle_8_10;
