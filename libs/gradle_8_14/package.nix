818574c0f17f35942f556790c437e6c9593fec18
Mio <mio@mio19.uk>
2026-05-26 19:31:53 +1000

    save

--- libs/gradle_8_14/package.nix
+++ libs/gradle_8_14/package.nix
@@ -3,13 +3,23 @@
   jdk21_headless,
   gradle-from-source,
   gradle_8_14_M8,
+  stdenv,
+  gradle-packages,
 }:
-gradle-from-source {
-  version = "8.14";
-  hash = "sha256-Dbyx8ya7mxxQc8CV8lolifU5xzqDPqW2eHGUqpLODJM=";
-  lockFile = ./gradle.lock;
-  defaultJava = jdk21_headless;
-  buildJdk = jdk17_headless;
-  # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.14-milestone-8
-  bootstrapGradle = gradle_8_14_M8;
-}
+if stdenv.isDarwin then
+  # darwin only: cannot build
+  (gradle-packages.mkGradle {
+    version = "8.14";
+    hash = "";
+    defaultJava = jdk21_headless;
+  }).wrapped
+else
+  gradle-from-source {
+    version = "8.14";
+    hash = "sha256-Dbyx8ya7mxxQc8CV8lolifU5xzqDPqW2eHGUqpLODJM=";
+    lockFile = ./gradle.lock;
+    defaultJava = jdk21_headless;
+    buildJdk = jdk17_headless;
+    # nix run github:tadfisher/gradle2nix/53672d5e875235c34dee1a4c012b0269ba76e440  -- --gradle-wrapper=8.14-milestone-8
+    bootstrapGradle = gradle_8_14_M8;
+  }
