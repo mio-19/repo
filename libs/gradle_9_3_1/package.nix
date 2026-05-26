17277a0e72a1fc15e6fdf5fde6b6453bed519e6b
Mio <mio@mio19.uk>
2026-05-26 19:48:04 +1000

    save

--- libs/gradle_9_3_1/package.nix
+++ libs/gradle_9_3_1/package.nix
@@ -3,14 +3,24 @@
   jdk21_headless,
   gradle-from-source,
   gradle_9_1_0,
+  stdenv,
+  gradle-packages,
 }:
-gradle-from-source {
-  version = "9.3.1";
-  hash = "sha256-uDc2w+D/xxK/2rguf48eUZ9UPYtVpMePfnJOKh/NNCE=";
-  lockFile = ./gradle.lock;
-  defaultJava = jdk21_headless;
-  buildJdk = jdk17_headless;
-  # nix run github:tadfisher/gradle2nix/v2 -- --gradle-wrapper=9.1.0
-  bootstrapGradle = gradle_9_1_0;
-  configureOnDemand = true;
-}
+if stdenv.isDarwin then
+  # darwin only: cannot build
+  (gradle-packages.mkGradle {
+    version = "9.3.1";
+    hash = "";
+    defaultJava = jdk21_headless;
+  }).wrapped
+else
+  gradle-from-source {
+    version = "9.3.1";
+    hash = "sha256-uDc2w+D/xxK/2rguf48eUZ9UPYtVpMePfnJOKh/NNCE=";
+    lockFile = ./gradle.lock;
+    defaultJava = jdk21_headless;
+    buildJdk = jdk17_headless;
+    # nix run github:tadfisher/gradle2nix/v2 -- --gradle-wrapper=9.1.0
+    bootstrapGradle = gradle_9_1_0;
+    configureOnDemand = true;
+  }
