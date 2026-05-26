bb89da047549df3dae3e42d28c3f48ebb4ae42ff
Mio <mio@mio19.uk>
2026-05-26 21:33:45 +1000

    save

--- libs/gradle_9_1_0/package.nix
+++ libs/gradle_9_1_0/package.nix
@@ -3,14 +3,24 @@
   jdk21_headless,
   gradle-from-source,
   gradle_9_0_0_M10,
+  stdenv,
+  gradle-packages,
 }:
-gradle-from-source {
-  version = "9.1.0";
-  hash = "sha256-jh54xQwXeWSQjjYUbhyZHEIOXATKJUyYfmnqdkXmW+8=";
-  lockFile = ./gradle.lock;
-  defaultJava = jdk21_headless;
-  buildJdk = jdk17_headless;
-  # nix run github:tadfisher/gradle2nix/v2 -- --gradle-dist=https://services.gradle.org/distributions/gradle-9.0.0-milestone-10-bin.zip
-  bootstrapGradle = gradle_9_0_0_M10;
-  configureOnDemand = true;
-}
+if stdenv.isDarwin then
+  # darwin only: cannot build
+  (gradle-packages.mkGradle {
+    version = "9.1.0";
+    hash = "sha256-oX3dhaJran9d23H/iwX8UQTAICxuZHgkKXkMkzaGyAY=";
+    defaultJava = jdk21_headless;
+  }).wrapped
+else
+  gradle-from-source {
+    version = "9.1.0";
+    hash = "sha256-jh54xQwXeWSQjjYUbhyZHEIOXATKJUyYfmnqdkXmW+8=";
+    lockFile = ./gradle.lock;
+    defaultJava = jdk21_headless;
+    buildJdk = jdk17_headless;
+    # nix run github:tadfisher/gradle2nix/v2 -- --gradle-dist=https://services.gradle.org/distributions/gradle-9.0.0-milestone-10-bin.zip
+    bootstrapGradle = gradle_9_0_0_M10;
+    configureOnDemand = true;
+  }
