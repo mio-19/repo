97850a266be029bb53edffba8497c80b0da48f1c
Mio <mio@mio19.uk>
2026-07-12 10:00:54 +1200

    url = "github:tadfisher/android-nixpkgs/2026-07-11-stable";

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     android-nixpkgs = {
       #url = "github:tadfisher/android-nixpkgs/stable";
       # this thing cause rebuild with no real thing changed everyday. pin.
-      url = "github:tadfisher/android-nixpkgs/2026-07-09-stable";
+      url = "github:tadfisher/android-nixpkgs/2026-07-11-stable";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
