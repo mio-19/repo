78e6d7eaeb0ea64f65ea055264f09998047c2854
Mio <mio@mio19.uk>
2026-08-14 20:31:57 +1000

    unbump

--- flake.nix
+++ flake.nix
@@ -166,6 +166,11 @@
             src = inputs.nixpkgs;
             name = "nixpkgs-patched";
             patches = [
+              (fetchpatch {
+                name = "fdroidserver: fix build, 2.4.3 -> 2.4.5";
+                url = "https://github.com/NixOS/nixpkgs/pull/548263.diff";
+                hash = "sha256-oARICQHIRDMsm4PSI8H4hIXdudNzkMrJJWOHJDPGOcw=";
+              })
               /*
                 # conflicts with https://github.com/NixOS/nixpkgs/pull/506356
                 (fetchpatch {
