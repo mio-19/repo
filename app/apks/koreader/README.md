a3b0e8750a9a59ee7c83681320a3662e472da741
Mio <mio@mio19.uk>
2026-08-17 19:11:46 +1000

    refreshed

--- app/apks/koreader/README.md
+++ app/apks/koreader/README.md
@@ -4,7 +4,7 @@ use gits.py in koreader repo to get repos.nix
 ```zsh
 git clone --recurse-submodules  https://github.com/koreader/koreader.git
 cd koreader
-git checkout v2026.03
+git checkout v2026.07.1
 git submodule update --init --recursive
 python3 ../gits.py > ../repos.nix   
 ```
--- app/apks/koreader/repos.nix
+++ app/apks/koreader/repos.nix
@@ -1,99 +1,8 @@
 { fetchgit }: [
   (fetchgit {
-    url = "https://github.com/koreader/libk2pdfopt.git";
-    rev = "1fcffeb1e61ec43265d23e2ac77d9a901f8c45d2";
-    hash = "sha256-CuKpGOI7MfoQOsaeuF+8f2muWfXDoul0l3wgqjCQXYQ=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/NiLuJe/FBInk.git";
-    rev = "92e127008145b2a22fba7c59815d810d716310dd";
-    hash = "sha256-yUdEsFi/9OLKTtKjmldbWMzDsYX644/uIGixFAVamkw=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/lunarmodules/luafilesystem";
-    tag = "v1_9_0";
-    hash = "sha256-aabznj5k6TMx153VeDBFedv7tFZzvgOkwo4yIEPy0t8=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/famzah/popen-noshell.git";
-    rev = "e715396a4951ee91c40a98d2824a130f158268bb";
-    hash = "sha256-oIAL6fMw6UfjG2CnV5zK1cZIkCdt68hEbq45dTRpSEM=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/notmarek/openlipclua.git";
-    rev = "96c2d16696a482664b4e84eb3b6d851f807a44d1";
-    hash = "sha256-oHHMXCZVuZp2CTqUOOjZ6UiDNK1eSa1w9TSDgi7+Yis=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/koreader/lj-wpaclient.git";
-    rev = "0d5c8ee336b699dbb35850a7336d24e89e1aac24";
-    hash = "sha256-m50+7D+LWl5LTqJttNUrHZnQM1FFXS9RbLglOuR9EQQ=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/kernelsauce/turbo";
-    tag = "v2.1.3";
-    hash = "sha256-4QbgBs/sUlaQ/SBQMUm/yvWnrjEUR1tQf1CHW8POO2g=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/koreader/pocketbook-inkview.git";
-    rev = "4a3e32c39ed14ac7f2e3ca4f8c66a7d5fbe5e0fe";
-    hash = "sha256-xq9JsxLcENBWt0rjDGhu/itT8oPyd5LG2K/TWBiZQSk=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/lunarmodules/luasocket";
-    rev = "e3ca4a767a68d127df548d82669aba3689bd84f4";
-    hash = "sha256-OFz07+Wdhiz/JhQ7AZjsklzM15Z9kJz2KUQpvcVnGsk=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/nmoinvaz/minizip";
-    rev = "aa4758baea16e9dc03583340e62b370ea1140df9";
-    hash = "sha256-scxgbWbZUTKRhbTeYCWkm7CLp0NOkGHSZleZHEv8VAc=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
     url = "https://github.com/NiLuJe/lua-rapidjson";
     rev = "e84973356255bde06a70ce6263a3a0ef5c8f4ad4";
     hash = "sha256-lq50sK+snOTpCnIMpJFh5+85SUX6/WjalOpWnZbkH4o=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/harningt/luajson.git";
-    rev = "6ecaf9bea8b121a9ffca5a470a2080298557b55d";
-    hash = "sha256-Y2GpaMWXoiGtbjlO8/NP/35PgRuzTkwXmxIetE8D+IM=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/brunoos/luasec";
-    tag = "v1.3.2";
-    hash = "sha256-sZu6acBs3Gy1h74sK7wU2NvPOd3kDaqeIeBKJ/pJv/E=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/memononen/nanosvg.git";
-    rev = "ea6a6aca009422bba0dbad4c80df6e6ba0c82183";
-    hash = "sha256-zop4QF6gawQjjsVPb9xqfMqUSawACR6/qjArT1Noqu4=";
     leaveDotGit = true;
     fetchSubmodules = true;
   })
@@ -105,30 +14,16 @@
     fetchSubmodules = true;
   })
   (fetchgit {
-    url = "https://framagit.org/fperrad/lua-Spore";
-    tag = "0.4.2";
-    hash = "sha256-wjXF7oEpdcpt8jmCH2bVj6PijOQpkHJu+22+MS/AbYo=";
+    url = "https://github.com/brunoos/luasec";
+    tag = "v1.3.2";
+    hash = "sha256-sZu6acBs3Gy1h74sK7wU2NvPOd3kDaqeIeBKJ/pJv/E=";
     leaveDotGit = true;
     fetchSubmodules = true;
   })
   (fetchgit {
-    url = "https://github.com/koreader/KoboUSBMS.git";
-    rev = "d1d916655b1406780f12ba0c2442b6a901e75929";
-    hash = "sha256-jYux6/xTC7CISi6TqN9EgQQafGJZNZPtwgdaozXjN3k=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/lvandeve/lodepng.git";
-    rev = "0b1d9ccfc2093e5d6620cd9a11d03ee6ff6705f5";
-    hash = "sha256-wskSBmAB/jT+t+ArGtLghSbdUAty9gbon8wMnpIeiYU=";
-    leaveDotGit = true;
-    fetchSubmodules = true;
-  })
-  (fetchgit {
-    url = "https://github.com/NiLuJe/zsync2.git";
-    rev = "e281e1eb4466ff6b3866c25dbe62a3e150fa5bfd";
-    hash = "sha256-p7nV17YfSPGi5A/L4h/T9HQJOmMqQNZv+iKvi3N2R9Y=";
+    url = "https://github.com/lunarmodules/luafilesystem";
+    tag = "v1_9_0";
+    hash = "sha256-aabznj5k6TMx153VeDBFedv7tFZzvgOkwo4yIEPy0t8=";
     leaveDotGit = true;
     fetchSubmodules = true;
   })
@@ -140,9 +35,107 @@
     fetchSubmodules = true;
   })
   (fetchgit {
+    url = "https://github.com/famzah/popen-noshell.git";
+    rev = "e715396a4951ee91c40a98d2824a130f158268bb";
+    hash = "sha256-oIAL6fMw6UfjG2CnV5zK1cZIkCdt68hEbq45dTRpSEM=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/koreader/lj-wpaclient.git";
+    rev = "0d5c8ee336b699dbb35850a7336d24e89e1aac24";
+    hash = "sha256-m50+7D+LWl5LTqJttNUrHZnQM1FFXS9RbLglOuR9EQQ=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/lunarmodules/luasocket";
+    rev = "e3ca4a767a68d127df548d82669aba3689bd84f4";
+    hash = "sha256-OFz07+Wdhiz/JhQ7AZjsklzM15Z9kJz2KUQpvcVnGsk=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/lvandeve/lodepng.git";
+    rev = "0b1d9ccfc2093e5d6620cd9a11d03ee6ff6705f5";
+    hash = "sha256-wskSBmAB/jT+t+ArGtLghSbdUAty9gbon8wMnpIeiYU=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
     url = "https://github.com/msva/lua-htmlparser";
     rev = "5ce9a775a345cf458c0388d7288e246bb1b82bff";
     hash = "sha256-kMhS7uUVeYRYrJU7VsbRH0aC54F/jaVWh7pdelLmasQ=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/harningt/luajson.git";
+    rev = "6ecaf9bea8b121a9ffca5a470a2080298557b55d";
+    hash = "sha256-Y2GpaMWXoiGtbjlO8/NP/35PgRuzTkwXmxIetE8D+IM=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/nmoinvaz/minizip";
+    rev = "aa4758baea16e9dc03583340e62b370ea1140df9";
+    hash = "sha256-scxgbWbZUTKRhbTeYCWkm7CLp0NOkGHSZleZHEv8VAc=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://framagit.org/fperrad/lua-Spore";
+    tag = "0.4.2";
+    hash = "sha256-wjXF7oEpdcpt8jmCH2bVj6PijOQpkHJu+22+MS/AbYo=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/koreader/pocketbook-inkview.git";
+    rev = "4a3e32c39ed14ac7f2e3ca4f8c66a7d5fbe5e0fe";
+    hash = "sha256-xq9JsxLcENBWt0rjDGhu/itT8oPyd5LG2K/TWBiZQSk=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/memononen/nanosvg.git";
+    rev = "ea6a6aca009422bba0dbad4c80df6e6ba0c82183";
+    hash = "sha256-zop4QF6gawQjjsVPb9xqfMqUSawACR6/qjArT1Noqu4=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/NiLuJe/FBInk.git";
+    rev = "92e127008145b2a22fba7c59815d810d716310dd";
+    hash = "sha256-yUdEsFi/9OLKTtKjmldbWMzDsYX644/uIGixFAVamkw=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/notmarek/openlipclua.git";
+    rev = "96c2d16696a482664b4e84eb3b6d851f807a44d1";
+    hash = "sha256-oHHMXCZVuZp2CTqUOOjZ6UiDNK1eSa1w9TSDgi7+Yis=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/NiLuJe/zsync2.git";
+    rev = "e281e1eb4466ff6b3866c25dbe62a3e150fa5bfd";
+    hash = "sha256-p7nV17YfSPGi5A/L4h/T9HQJOmMqQNZv+iKvi3N2R9Y=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/koreader/libk2pdfopt.git";
+    rev = "1fcffeb1e61ec43265d23e2ac77d9a901f8c45d2";
+    hash = "sha256-CuKpGOI7MfoQOsaeuF+8f2muWfXDoul0l3wgqjCQXYQ=";
+    leaveDotGit = true;
+    fetchSubmodules = true;
+  })
+  (fetchgit {
+    url = "https://github.com/kernelsauce/turbo";
+    tag = "v2.1.3";
+    hash = "sha256-4QbgBs/sUlaQ/SBQMUm/yvWnrjEUR1tQf1CHW8POO2g=";
     leaveDotGit = true;
     fetchSubmodules = true;
   })
