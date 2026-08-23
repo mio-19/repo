da668bb5e01f497d76cc89d261105358d4df8eb5
Mio <mio@mio19.uk>
2026-08-23 16:53:58 +1000

    Revert "bump"
    This reverts commit fb1724515370d1128456444cd16602ceaa905ced.

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     android-nixpkgs = {
       #url = "github:tadfisher/android-nixpkgs/stable";
       # this thing cause rebuild with no real thing changed everyday. pin.
-      url = "github:tadfisher/android-nixpkgs/2026-08-22-stable";
+      url = "github:tadfisher/android-nixpkgs/2026-08-18-stable";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
